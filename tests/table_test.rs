fcb8f86acdbd0e0da2cb9af038cb9e1f1eb5a1c0
Maxim Zhiburt <zhiburt@gmail.com>
2020-02-25 16:22:32 +0300

    testing: add test for table method with vector of structures

---
+++ tests/table_test.rs
@@ -0,0 +1,23 @@
+use tabled::{table, Tabled};
+
+#[test]
+fn table_vector_structures() {
+    #[derive(Tabled)]
+    struct St {
+        f1: u8,
+        f2: &'static str,
+    }
+
+    let st = vec![St { f1: 0, f2: "0" }, St { f1: 1, f2: "1" }];
+    let expected = "+----+----+\n\
+                    | f1 | f2 |\n\
+                    +----+----+\n\
+                    | 0  | 0  |\n\
+                    +----+----+\n\
+                    | 1  | 1  |\n\
+                    +----+----+\n";
+
+    let table = table(st);
+
+    assert_eq!(expected, table);
+}
