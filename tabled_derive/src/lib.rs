69642b8804b5817a78a19f3b332d05da770756b4
Maxim Zhiburt <zhiburt@gmail.com>
2021-06-21 13:47:04 +0300

    Refactoring of tabled_derive

--- tabled_derive/src/lib.rs
+++ tabled_derive/src/lib.rs
@@ -14,10 +14,8 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::*;
-use syn::{
-    parse_macro_input, Attribute, DeriveInput, Field, Lit,
-    Meta, NestedMeta,
-};
+use std::str;
+use syn::{parse_macro_input, Attribute, DeriveInput, Field, Lit, Meta, NestedMeta};
 
 #[proc_macro_derive(Tabled, attributes(header, field))]
 pub fn tabled(input: TokenStream) -> TokenStream {
@@ -63,42 +61,17 @@ fn get_st_headers(st: &syn::DataStruct) -> Vec<String> {
 fn get_fields_headers<'a>(fields: impl Iterator<Item = &'a Field>) -> Vec<String> {
     fields
         .enumerate()
-        .filter(|(_, f)| {
-            let is_ignored = find_bool_attribute(&f.attrs, "header", "hidden");
-            is_ignored != Some(true)
-        })
-        .map(|(i, f)| {
-            let override_name = find_name_attribute(&f.attrs, "header", "name", true);
-            match override_name {
-                Some(name) => name,
-                None => f
-                    .ident
-                    .as_ref()
-                    .map_or_else(|| format!("{}", i), |f| f.to_string()),
-            }
-        })
+        .filter(|(_, f)| !is_ignored_field(f))
+        .map(|(i, f)| field_name(f, i))
         .collect()
 }
 
 fn get_enum_headers(e: &syn::DataEnum) -> Vec<String> {
     e.variants
         .iter()
-        .filter(|v| {
-            let is_ignored = find_bool_attribute(&v.attrs, "header", "hidden");
-            is_ignored != Some(true)
-        })
-        .map(|v| {
-            let override_name = find_name_attribute(&v.attrs, "header", "name", true);
-            match override_name {
-                Some(name) => vec![name],
-                None => {
-                    let variant = v.ident.to_string();
-                    vec![format!("{}", variant)]
-                }
-            }
-        })
-        .collect::<Vec<Vec<_>>>()
-        .concat()
+        .filter(|v| !is_ignored_variant(v))
+        .map(variant_name)
+        .collect()
 }
 
 fn get_fields(d: &syn::Data) -> proc_macro2::TokenStream {
@@ -115,7 +88,8 @@ fn get_fields(d: &syn::Data) -> proc_macro2::TokenStream {
 fn get_st_fields(st: &syn::DataStruct) -> Vec<proc_macro2::TokenStream> {
     let mut v = Vec::new();
     for (i, field) in st.fields.iter().enumerate() {
-        let is_ignored = find_bool_attribute(&field.attrs, "header", "hidden");
+        let is_ignored =
+            find_name_attribute(&field.attrs, "header", "hidden", look_up_nested_meta_bool);
         if is_ignored == Some(true) {
             continue;
         }
@@ -129,7 +103,12 @@ fn get_st_fields(st: &syn::DataStruct) -> Vec<proc_macro2::TokenStream> {
             |f| quote!(self.#f),
         );
 
-        let with_function = find_name_attribute(&field.attrs, "field", "display_with", false);
+        let with_function = find_name_attribute(
+            &field.attrs,
+            "field",
+            "display_with",
+            look_up_nested_meta_str,
+        );
         if let Some(function) = with_function {
             let function = syn::Ident::new(&function, proc_macro2::Span::call_site());
             value = quote! { #function(&#value) };
@@ -146,10 +125,7 @@ fn get_enum_fields(e: &syn::DataEnum) -> proc_macro2::TokenStream {
     let mut variant_field_shift = Vec::new();
     let mut variant_fields_len = Vec::new();
     let mut count_fields = 0;
-    let variants = e.variants.iter().filter(|v| {
-        let is_ignored = find_bool_attribute(&v.attrs, "header", "hidden");
-        is_ignored != Some(true)
-    });
+    let variants = e.variants.iter().filter(|v| !is_ignored_variant(v));
     for _ in variants {
         let fields = vec![quote! { "+".to_string() }];
 
@@ -162,10 +138,7 @@ fn get_enum_fields(e: &syn::DataEnum) -> proc_macro2::TokenStream {
     let variants = e
         .variants
         .iter()
-        .filter(|v| {
-            let is_ignored = find_bool_attribute(&v.attrs, "header", "hidden");
-            is_ignored != Some(true)
-        })
+        .filter(|v| !is_ignored_variant(v))
         .map(|v| {
             let mut token = proc_macro2::TokenStream::new();
             token.append_all(v.ident.to_token_stream());
@@ -218,90 +191,114 @@ fn get_enum_fields(e: &syn::DataEnum) -> proc_macro2::TokenStream {
     }
 }
 
-fn parse_name_attribute(attr: &Attribute, method: &str, name: &str, flaged: bool) -> Option<String> {
-    if attr.path.is_ident(method) {
-        let meta = &attr.parse_meta();
-        match meta {
-            Ok(Meta::List(meta_list)) => {
-                if meta_list.path.is_ident(method) {
-                    for nested_meta in &meta_list.nested {
-                        match nested_meta {
-                            NestedMeta::Lit(Lit::Str(value)) if flaged => return Some(value.value()),
-                            NestedMeta::Lit(Lit::ByteStr(value)) if flaged => return Some(
-                                std::str::from_utf8(&value.value())
-                                    .expect(&format!("Expected a valid UTF-8 string for a macro {macro} field {name}", macro=method, name=name))
-                                    .to_owned(),
-                            ),
-                            NestedMeta::Meta(Meta::NameValue(value)) => {
-                                if value.path.is_ident(name) {
-                                    match &value.lit {
-                                        Lit::Str(value) => return Some(value.value()),
-                                        Lit::ByteStr(value) => return Some(
-                                            std::str::from_utf8(&value.value())
-                                                .expect(&format!("Expected a valid UTF-8 string for a macro {macro} field {name}", macro=method, name=name))
-                                                .to_owned(),
-                                        ),
-                                        _ => panic!("Parameter {name} for macro {macro} should be String", name=name, macro=method)
-
-                                    }
-                                }
-                            }
-                            _ => {
-                                
-                            }
-                        }
-                    }
-                }
-
-                None
-            }
-            _ => None,
-        }
-    } else {
-        None
+fn field_name(f: &syn::Field, index: usize) -> String {
+    let override_name = find_name_attribute(&f.attrs, "header", "name", look_up_nested_meta_str)
+        .or_else(|| find_name_attribute(&f.attrs, "header", "name", look_up_nested_meta_flag_str));
+    match override_name {
+        Some(name) => name,
+        None => match f.ident.as_ref() {
+            Some(name) => name.to_string(),
+            None => format!("{}", index),
+        },
     }
 }
 
-fn find_name_attribute(attributes: &[Attribute], method: &str, name: &str, flaged: bool) -> Option<String> {
-    attributes
-        .iter()
-        .find_map(|attr| parse_name_attribute(attr, method, name, flaged))
+fn variant_name(v: &syn::Variant) -> String {
+    find_name_attribute(&v.attrs, "header", "name", look_up_nested_meta_str)
+        .or_else(|| find_name_attribute(&v.attrs, "header", "name", look_up_nested_meta_flag_str))
+        .unwrap_or_else(|| v.ident.to_string())
 }
 
-fn parse_bool_attribute(attr: &Attribute, method: &str, name: &str) -> Option<bool> {
-    if attr.path.is_ident(method) {
-        let meta = &attr.parse_meta();
-        match meta {
-            Ok(Meta::List(meta_list)) => {
-                for nested_meta in &meta_list.nested {
-                    match nested_meta {
-                        NestedMeta::Meta(Meta::Path(path)) => if path.is_ident(name) { return Some(true) } else { return None },
-                        NestedMeta::Meta(Meta::NameValue(value)) => {
-                            if value.path.is_ident(name) {
-                                match &value.lit {
-                                    Lit::Bool(value) => return Some(value.value()),
-                                    Lit::Verbatim(literal) => panic!("{:?}", literal),
-                                    _ => panic!("Parameter {name} for macro {macro} should be a bool value", name=name, macro=method)
-                                }
-                            }
-                        }
-                        _ => {
-                            
-                        }
-                    }
-                }
+fn is_ignored_field(f: &syn::Field) -> bool {
+    attrs_has_ignore_sign(&f.attrs)
+}
 
-                None
+fn is_ignored_variant(f: &syn::Variant) -> bool {
+    attrs_has_ignore_sign(&f.attrs)
+}
+
+fn attrs_has_ignore_sign(attrs: &[syn::Attribute]) -> bool {
+    let is_ignored = find_name_attribute(&attrs, "header", "hidden", look_up_nested_meta_bool);
+    is_ignored == Some(true)
+}
+
+fn parse_name_attribute<R, F>(attr: &Attribute, method: &str, name: &str, lookup: F) -> Option<R>
+where
+    F: Fn(&syn::NestedMeta, &str) -> Result<Option<R>, String>,
+{
+    if !attr.path.is_ident(method) {
+        return None;
+    }
+
+    let meta = attr.parse_meta().ok()?;
+
+    if let Meta::List(meta_list) = meta {
+        for nested_meta in &meta_list.nested {
+            let val = lookup(nested_meta, name).unwrap_or_else(
+                |e| panic!("{error} macros {macro} field {name}", error=e, macro=method, name=name),
+            );
+
+            if val.is_some() {
+                return val;
             }
-            _ => None,
         }
-    } else {
-        None
+    }
+
+    None
+}
+
+fn look_up_nested_meta_str(meta: &syn::NestedMeta, name: &str) -> Result<Option<String>, String> {
+    match meta {
+        NestedMeta::Meta(Meta::NameValue(value)) => {
+            if value.path.is_ident(name) {
+                check_str_literal(&value.lit)
+            } else {
+                Ok(None)
+            }
+        }
+        _ => Ok(None),
     }
 }
 
-fn find_bool_attribute(attributes: &[Attribute], method: &str, name: &str) -> Option<bool> {
+fn look_up_nested_meta_flag_str(meta: &syn::NestedMeta, _: &str) -> Result<Option<String>, String> {
+    match meta {
+        NestedMeta::Lit(lit) => check_str_literal(lit),
+        _ => Ok(None),
+    }
+}
+
+fn check_str_literal(lit: &syn::Lit) -> Result<Option<String>, String> {
+    match lit {
+        Lit::Str(value) => Ok(Some(value.value())),
+        Lit::ByteStr(value) => str::from_utf8(&value.value())
+            .map(|s| s.to_owned())
+            .map(Some)
+            .map_err(|_| "Expected a valid UTF-8 string for a field".to_owned()),
+        _ => Ok(None),
+    }
+}
+
+fn look_up_nested_meta_bool(meta: &syn::NestedMeta, name: &str) -> Result<Option<bool>, String> {
+    match meta {
+        NestedMeta::Meta(Meta::Path(path)) if path.is_ident(name) => Ok(Some(true)),
+        NestedMeta::Meta(Meta::NameValue(value)) if value.path.is_ident(name) => match &value.lit {
+            Lit::Bool(value) => Ok(Some(value.value())),
+            _ => Err("A parameter should be a bool value".to_string()),
+        },
+        _ => Ok(None),
+    }
+}
+
+fn find_name_attribute<R, F>(
+    attributes: &[Attribute],
+    method: &str,
+    name: &str,
+    lookup: F,
+) -> Option<R>
+where
+    F: Fn(&syn::NestedMeta, &str) -> Result<Option<R>, String> + Clone,
+{
     attributes
         .iter()
-        .find_map(|attr| parse_bool_attribute(attr, method, name))
+        .find_map(|attr| parse_name_attribute(attr, method, name, lookup.clone()))
 }
