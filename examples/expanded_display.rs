a8a8a57c0fe3975265f0efd57f128a6b4758b69e
Maxim Zhiburt <zhiburt@gmail.com>
2021-09-09 12:01:59 +0300

    Add example for ExpanededDisplay type
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ examples/expanded_display.rs
@@ -0,0 +1,49 @@
+//! The example can be run by this command
+//! `cargo run --example expanded_display`
+
+use tabled::{display::ExpandedDisplay, Tabled};
+
+#[derive(Tabled)]
+struct Distribution {
+    name: &'static str,
+    #[field(display_with = "Self::display_based_on")]
+    based_on: Option<&'static str>,
+    is_active: bool,
+    is_cool: bool,
+}
+
+impl Distribution {
+    fn display_based_on(o: &Option<&'static str>) -> String {
+        match o {
+            &Some(s) => s.into(),
+            None => "Independent".into(),
+        }
+    }
+}
+
+fn main() {
+    let data = [
+        Distribution {
+            name: "Manjaro",
+            based_on: Some("Arch"),
+            is_cool: true,
+            is_active: true,
+        },
+        Distribution {
+            name: "Debian",
+            based_on: None,
+            is_cool: true,
+            is_active: true,
+        },
+        Distribution {
+            name: "Debian",
+            based_on: None,
+            is_cool: true,
+            is_active: true,
+        },
+    ];
+
+    let table = ExpandedDisplay::new(&data);
+
+    println!("{}", table);
+}
