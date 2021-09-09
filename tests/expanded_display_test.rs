4ed08510167cef1f93e79acb7fbf41378fe6c4a6
Maxim Zhiburt <zhiburt@gmail.com>
2021-09-09 12:01:45 +0300

    Add test for ExpandedDisplay type
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ tests/expanded_display_test.rs
@@ -0,0 +1,129 @@
+use tabled::{display::ExpandedDisplay, Tabled};
+
+#[test]
+fn display() {
+    #[derive(Tabled)]
+    struct Linux {
+        id: u8,
+        destribution: &'static str,
+        link: &'static str,
+    }
+
+    let data = vec![
+        Linux {
+            id: 0,
+            destribution: "Fedora",
+            link: "https://getfedora.org/",
+        },
+        Linux {
+            id: 2,
+            destribution: "OpenSUSE",
+            link: "https://www.opensuse.org/",
+        },
+        Linux {
+            id: 3,
+            destribution: "Endeavouros",
+            link: "https://endeavouros.com/",
+        },
+    ];
+
+    let expected = concat!(
+        "-[ RECORD 0 ]-\n",
+        "id           | \"0\"\n",
+        "destribution | \"Fedora\"\n",
+        "link         | \"https://getfedora.org/\"\n",
+        "-[ RECORD 1 ]-\n",
+        "id           | \"2\"\n",
+        "destribution | \"OpenSUSE\"\n",
+        "link         | \"https://www.opensuse.org/\"\n",
+        "-[ RECORD 2 ]-\n",
+        "id           | \"3\"\n",
+        "destribution | \"Endeavouros\"\n",
+        "link         | \"https://endeavouros.com/\"\n",
+    );
+
+    let table = ExpandedDisplay::new(&data).to_string();
+
+    assert_eq!(table, expected);
+}
+
+#[test]
+fn display_multiline_field() {
+    #[allow(dead_code)]
+    struct Linux {
+        id: u8,
+        destribution: &'static str,
+        link: &'static str,
+    }
+
+    impl Tabled for Linux {
+        fn fields(&self) -> Vec<String> {
+            vec![String::new(), String::new(), String::new()]
+        }
+
+        fn headers() -> Vec<String> {
+            vec![
+                "Hello\nWorld".to_string(),
+                "123".to_string(),
+                "asd".to_string(),
+            ]
+        }
+    }
+
+    let data = vec![Linux {
+        id: 0,
+        destribution: "Fedora",
+        link: "https://getfedora.org/",
+    }];
+
+    let expected = concat!(
+        "-[ RECORD 0 ]-\n",
+        "Hello\\nWorld | \"\"\n",
+        "123          | \"\"\n",
+        "asd          | \"\"\n",
+    );
+
+    let table = ExpandedDisplay::new(&data).to_string();
+
+    assert_eq!(table, expected);
+}
+
+#[test]
+fn display_multiline_record_value() {
+    #[allow(dead_code)]
+    struct Linux {
+        id: u8,
+        destribution: &'static str,
+        link: &'static str,
+    }
+
+    impl Tabled for Linux {
+        fn fields(&self) -> Vec<String> {
+            vec![
+                "Hello\nWorld".to_string(),
+                "123".to_string(),
+                "asd".to_string(),
+            ]
+        }
+
+        fn headers() -> Vec<String> {
+            vec![String::new(), String::new(), String::new()]
+        }
+    }
+
+    let data = vec![Linux {
+        id: 0,
+        destribution: "Fedora",
+        link: "https://getfedora.org/",
+    }];
+
+    let expected = concat!(
+        "-[ RECORD 0 ]-\n",
+        " | \"Hello\\nWorld\"\n",
+        " | \"123\"\n | \"asd\"\n",
+    );
+
+    let table = ExpandedDisplay::new(&data).to_string();
+
+    assert_eq!(table, expected);
+}
