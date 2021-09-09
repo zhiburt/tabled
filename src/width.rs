a8aab8fc9b5495c903a82432cbb717572a7909b2
Maxim Zhiburt <zhiburt@gmail.com>
2021-09-09 20:04:07 +0300

    Refactoring
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/width.rs
+++ src/width.rs
@@ -1,11 +1,5 @@
 use crate::CellOption;
 use papergrid::{Entity, Grid, Settings};
-use unicode_segmentation::UnicodeSegmentation;
-
-enum Wrap<S> {
-    Truncate(S),
-    Wrap,
-}
 
 /// Using MaxWidth you can set a max width of an object on a [Grid].
 ///
@@ -26,46 +20,53 @@ enum Wrap<S> {
 /// ```
 ///
 /// While working with colors you must setup `colors` feature.
-pub struct MaxWidth<S>(usize, Wrap<S>);
+pub struct MaxWidth<S> {
+    width: usize,
+    wrap: Wrap<S>,
+}
+
+enum Wrap<S> {
+    Truncate(S),
+    Wrap,
+}
 
 impl<S> MaxWidth<S>
 where
     S: AsRef<str>,
 {
     pub fn truncating(width: usize, suffix: S) -> Self {
-        Self(width, Wrap::Truncate(suffix))
+        Self {
+            width,
+            wrap: Wrap::Truncate(suffix),
+        }
     }
 }
 
 impl MaxWidth<&'static str> {
     pub fn wrapping(width: usize) -> Self {
-        Self(width, Wrap::Wrap)
+        Self {
+            width,
+            wrap: Wrap::Wrap,
+        }
     }
 }
 
 impl<S: AsRef<str>> CellOption for MaxWidth<S> {
     fn change_cell(&mut self, grid: &mut Grid, row: usize, column: usize) {
-        let width = self.0;
-
         let content = grid.get_cell_content(row, column);
-
-        match &self.1 {
+        match &self.wrap {
             Wrap::Truncate(filler) => {
-                let striped_content = strip(content, width);
-                let old_content_length = content.len();
-                let new_content_length = striped_content.len();
-                if new_content_length != old_content_length {
-                    let content = format!("{}{}", striped_content, filler.as_ref());
-                    grid.set(Entity::Cell(row, column), Settings::new().text(content))
+                let striped_content = strip(content, self.width);
+                if striped_content.len() < content.len() {
+                    let new_content = format!("{}{}", striped_content, filler.as_ref());
+                    grid.set(Entity::Cell(row, column), Settings::new().text(new_content))
                 }
+        
             }
             Wrap::Wrap => {
-                let split_content = split(content, width);
-                let old_content_length = content.len();
-                let new_content_length = split_content.len();
-                if new_content_length != old_content_length {
-                    let content = format!("{}", split_content);
-                    grid.set(Entity::Cell(row, column), Settings::new().text(content))
+                let wrapped_content = split(content, self.width);
+                if wrapped_content.len() != content.len() {
+                    grid.set(Entity::Cell(row, column), Settings::new().text(wrapped_content))
                 }
             }
         }
@@ -85,11 +86,10 @@ fn strip(s: &str, width: usize) -> String {
 }
 
 fn split(s: &str, width: usize) -> String {
-    // TODO: consider colors here.
-    s.graphemes(true)
-        .collect::<Vec<&str>>()
+    s.chars()
+        .collect::<Vec<char>>()
         .chunks(width)
-        .map(|chunk| chunk.concat())
+        .map(|chunk| chunk.iter().collect::<String>())
         .collect::<Vec<String>>()
         .join("\n")
 }
