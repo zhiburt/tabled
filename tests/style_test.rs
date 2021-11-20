7cfbd5e23300717fdcffdc706ab6ac105f724ef7
Maxim Zhiburt <zhiburt@gmail.com>
2021-11-20 15:45:37 +0300

    tabled/ Add more highliting tests
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- tests/style_test.rs
+++ tests/style_test.rs
@@ -335,7 +335,7 @@ fn custom_style() {
 }
 
 #[test]
-fn style_highlingting() {
+fn style_highlingt_cell() {
     let data = vec![
         Linux {
             id: 0,
@@ -375,6 +375,109 @@ fn style_highlingting() {
                 )
                 .highlight(
                     Entity::Cell(1, 1),
+                    Border::full('*', '*', '*', '*', '*', '*', '*', '*'),
+                ),
+        )
+        .to_string();
+
+    println!("{}", table);
+
+    assert_eq!(table, expected);
+}
+
+#[test]
+fn style_highlingt_row() {
+    let data = vec![
+        Linux {
+            id: 0,
+            destribution: "Fedora",
+            link: "https://getfedora.org/",
+        },
+        Linux {
+            id: 2,
+            destribution: "OpenSUSE",
+            link: "https://www.opensuse.org/",
+        },
+        Linux {
+            id: 3,
+            destribution: "Endeavouros",
+            link: "https://endeavouros.com/",
+        },
+    ];
+
+    let expected = concat!(
+        "+++++++++++++++++++++++++++++++++++++++++++++++++\n",
+        "+ id │ destribution │           link            +\n",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++\n",
+        "│ 0  │    Fedora    │  https://getfedora.org/   │\n",
+        "├────┼──────────────┼───────────────────────────┤\n",
+        "│ 2  │   OpenSUSE   │ https://www.opensuse.org/ │\n",
+        "*************************************************\n",
+        "* 3  │ Endeavouros  │ https://endeavouros.com/  *\n",
+        "*************************************************\n",
+    );
+
+    let table = Table::new(&data)
+        .with(
+            Style::pseudo()
+                .highlight(
+                    Entity::Row(0),
+                    Border::full('+', '+', '+', '+', '+', '+', '+', '+'),
+                )
+                .highlight(
+                    Entity::Row(3),
+                    Border::full('*', '*', '*', '*', '*', '*', '*', '*'),
+                ),
+        )
+        .to_string();
+
+    println!("{}", table);
+
+    assert_eq!(table, expected);
+}
+
+
+#[test]
+fn style_highlingt_column() {
+    let data = vec![
+        Linux {
+            id: 0,
+            destribution: "Fedora",
+            link: "https://getfedora.org/",
+        },
+        Linux {
+            id: 2,
+            destribution: "OpenSUSE",
+            link: "https://www.opensuse.org/",
+        },
+        Linux {
+            id: 3,
+            destribution: "Endeavouros",
+            link: "https://endeavouros.com/",
+        },
+    ];
+
+    let expected = concat!(
+        "++++++──────────────*****************************\n",
+        "+ id + destribution *           link            *\n",
+        "+────+──────────────*───────────────────────────*\n",
+        "+ 0  +    Fedora    *  https://getfedora.org/   *\n",
+        "+────+──────────────*───────────────────────────*\n",
+        "+ 2  +   OpenSUSE   * https://www.opensuse.org/ *\n",
+        "+────+──────────────*───────────────────────────*\n",
+        "+ 3  + Endeavouros  * https://endeavouros.com/  *\n",
+        "++++++──────────────*****************************\n",
+    );
+
+    let table = Table::new(&data)
+        .with(
+            Style::pseudo()
+                .highlight(
+                    Entity::Column(0),
+                    Border::full('+', '+', '+', '+', '+', '+', '+', '+'),
+                )
+                .highlight(
+                    Entity::Column(2),
                     Border::full('*', '*', '*', '*', '*', '*', '*', '*'),
                 ),
         )
