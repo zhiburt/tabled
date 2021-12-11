f9c4cd07897a29fb8edf8f2b8b0116acbc329e5b
Senk <44151180+senk8@users.noreply.github.com>
2021-12-12 02:27:45 +0900

    tabled/ use create_vector and remove bottom and top

--- src/join.rs
+++ src/join.rs
@@ -4,34 +4,14 @@ use crate::TableOption;
 use papergrid::{Entity, Grid};
 
 pub enum Join {
-    VerticalTop(Table),
-    VerticalBottom(Table),
-    HorizontalLeft(Table),
-    HorizontalRight(Table),
+    Vertical(Table),
+    Horizontal(Table),
 }
 
 impl TableOption for Join {
     fn change(&mut self, other: &mut Grid) {
         match self {
-            Join::VerticalTop(table) => {
-                let new_row_size = table.grid.count_rows() + other.count_rows();
-                let mut new_grid = Grid::new(new_row_size,table.grid.count_rows());
-
-                for column in 0..new_grid.count_columns() {
-                    for row in 0..new_grid.count_rows() {
-                        let settings = if row < table.grid.count_rows() {
-                            table.grid.get_settings(row, column)
-                        } else {
-                            let row = row - table.grid.count_rows();
-                            other.get_settings(row, column)
-                        };
-                        new_grid.set(&Entity::Cell(row,column),settings.border_restriction(false));
-                    }
-                }
-            
-                *other = new_grid;
-            },
-            Join::VerticalBottom(table) => {
+            Join::Vertical(table) => {
                 let new_row_size = table.grid.count_rows() + other.count_rows();
                 let mut new_grid = Grid::new(new_row_size,table.grid.count_rows());
 
@@ -49,25 +29,7 @@ impl TableOption for Join {
 
                 *other = new_grid;
             },
-            Join::HorizontalLeft(table) => {
-                let new_column_size = table.grid.count_columns() + other.count_columns();
-                let mut new_grid = Grid::new(table.grid.count_rows(),new_column_size);
-
-                for column in 0..new_grid.count_columns() {
-                    for row in 0..new_grid.count_rows() {
-                        let settings = if column < table.grid.count_columns() {
-                            table.grid.get_settings(row, column)
-                        } else {
-                            let column = column - table.grid.count_columns();
-                            other.get_settings(row, column)
-                        };
-                        new_grid.set(&Entity::Cell(row,column),settings.border_restriction(false));
-                    }
-                }
-            
-                *other = new_grid;
-            },
-            Join::HorizontalRight(table) => {
+            Join::Horizontal(table) => {
                 let new_column_size = table.grid.count_columns() + other.count_columns();
                 let mut new_grid = Grid::new(table.grid.count_rows(),new_column_size);
 
--- tests/join_test.rs
+++ tests/join_test.rs
@@ -1,196 +1,135 @@
-use tabled::{Style, Table, Tabled, join};
+use tabled::{Style, Table, join, Full, Modify, Alignment};
+use util::create_vector;
 
 mod util;
 
 #[test]
 fn table_join_vertical_top() {
-    #[derive(Tabled)]
-    enum Domain {
-        Security,
-        Embeded,
-        Frontend,
-        Unknown,
-    }
+    let data1 = create_vector::<3, 3>();
+    let data2 = create_vector::<3, 3>();
 
-    #[derive(Tabled)]
-    struct Developer(#[header("name")] &'static str);
+    let table1 = Table::new(&data1)
+        .with(Style::PSQL)
+        .with(Modify::new(Full).with(Alignment::left()));
 
-    let data1 = vec![
-        (Developer("Terri Kshlerin"), Domain::Embeded),
-        (Developer("Catalina Dicki"), Domain::Security),
-        (Developer("Jennie Schmeler"), Domain::Frontend),
-        (Developer("Maxim Zhiburt"), Domain::Unknown),
-    ];
+    let table2 = Table::new(&data2)
+        .with(Style::ASCII)
+        .with(Modify::new(Full).with(Alignment::left()));
+        
+    let table3 = table1.with(join::Join::Vertical(table2));
 
-    let table1 = Table::new(data1).with(Style::psql());
-
-    let data2 = vec![
-        (Developer("Taro Yamada"), Domain::Embeded),
-        (Developer("Hanako Sato"), Domain::Security),
-        (Developer("Ken Nakamura"), Domain::Unknown),
-        (Developer("Hiro Takahashi"), Domain::Frontend),
-    ];
-
-    let table2 = Table::new(data2).with(Style::psql());
-    let table3 = table1.with(join::Join::VerticalTop(table2));
-
-    assert_eq!(
-        table3.to_string(),
-        concat!(
-            "      name       | Security | Embeded | Frontend | Unknown \n",
-            "-----------------+----------+---------+----------+---------\n",
-            "   Taro Yamada   |          |    +    |          |         \n",
-            "   Hanako Sato   |    +     |         |          |         \n",
-            "  Ken Nakamura   |          |         |          |    +    \n",
-            " Hiro Takahashi  |          |         |    +     |         \n",
-            "      name       | Security | Embeded | Frontend | Unknown \n",
-            "-----------------+----------+---------+----------+---------\n",
-            " Terri Kshlerin  |          |    +    |          |         \n",
-            " Catalina Dicki  |    +     |         |          |         \n",
-            " Jennie Schmeler |          |         |    +     |         \n",
-            "  Maxim Zhiburt  |          |         |          |    +    \n",
-        )
+    let expected = concat!(
+        "  N | column 0 | column 1 | column 2  \n",
+        " ---+----------+----------+---------- \n",
+        "  0 | 0-0      | 0-1      | 0-2       \n",
+        "  1 | 1-0      | 1-1      | 1-2       \n",
+        "  2 | 2-0      | 2-1      | 2-2       \n",
+        "+---+----------+----------+----------+\n",
+        "| N | column 0 | column 1 | column 2 |\n",
+        "+---+----------+----------+----------+\n",
+        "| 0 | 0-0      | 0-1      | 0-2      |\n",
+        "+---+----------+----------+----------+\n",
+        "| 1 | 1-0      | 1-1      | 1-2      |\n",
+        "+---+----------+----------+----------+\n",
+        "| 2 | 2-0      | 2-1      | 2-2      |\n",
+        "+---+----------+----------+----------+\n",
     );
+
+    assert_eq!(table3.to_string(), expected);
 }
 
 #[test]
 fn table_join_vertical_bottom() {
-    #[derive(Tabled)]
-    enum Domain {
-        Security,
-        Embeded,
-        Frontend,
-        Unknown,
-    }
+    let data1 = create_vector::<3, 3>();
+    let data2 = create_vector::<3, 3>();
 
-    #[derive(Tabled)]
-    struct Developer(#[header("name")] &'static str);
+    let table1 = Table::new(&data1)
+        .with(Style::PSQL)
+        .with(Modify::new(Full).with(Alignment::left()));
 
-    let data1 = vec![
-        (Developer("Terri Kshlerin"), Domain::Embeded),
-        (Developer("Catalina Dicki"), Domain::Security),
-        (Developer("Jennie Schmeler"), Domain::Frontend),
-        (Developer("Maxim Zhiburt"), Domain::Unknown),
-    ];
+    let table2 = Table::new(&data2)
+        .with(Style::ASCII)
+        .with(Modify::new(Full).with(Alignment::left()));
+        
+    let table3 = table2.with(join::Join::Vertical(table1));
 
-    let table1 = Table::new(data1).with(Style::psql());
-
-    let data2 = vec![
-        (Developer("Taro Yamada"), Domain::Embeded),
-        (Developer("Hanako Sato"), Domain::Security),
-        (Developer("Ken Nakamura"), Domain::Unknown),
-        (Developer("Hiro Takahashi"), Domain::Frontend),
-    ];
-
-    let table2 = Table::new(data2).with(Style::psql());
-    let table3 = table1.with(join::Join::VerticalBottom(table2));
-
-    assert_eq!(
-        table3.to_string(),
-        concat!(
-            "      name       | Security | Embeded | Frontend | Unknown \n",
-            "-----------------+----------+---------+----------+---------\n",
-            " Terri Kshlerin  |          |    +    |          |         \n",
-            " Catalina Dicki  |    +     |         |          |         \n",
-            " Jennie Schmeler |          |         |    +     |         \n",
-            "  Maxim Zhiburt  |          |         |          |    +    \n",
-            "      name       | Security | Embeded | Frontend | Unknown \n",
-            "-----------------+----------+---------+----------+---------\n",
-            "   Taro Yamada   |          |    +    |          |         \n",
-            "   Hanako Sato   |    +     |         |          |         \n",
-            "  Ken Nakamura   |          |         |          |    +    \n",
-            " Hiro Takahashi  |          |         |    +     |         \n",
-        )
+    let expected = concat!(
+        "+---+----------+----------+----------+\n",
+        "| N | column 0 | column 1 | column 2 |\n",
+        "+---+----------+----------+----------+\n",
+        "| 0 | 0-0      | 0-1      | 0-2      |\n",
+        "+---+----------+----------+----------+\n",
+        "| 1 | 1-0      | 1-1      | 1-2      |\n",
+        "+---+----------+----------+----------+\n",
+        "| 2 | 2-0      | 2-1      | 2-2      |\n",
+        "+---+----------+----------+----------+\n",
+        "  N | column 0 | column 1 | column 2  \n",
+        " ---+----------+----------+---------- \n",
+        "  0 | 0-0      | 0-1      | 0-2       \n",
+        "  1 | 1-0      | 1-1      | 1-2       \n",
+        "  2 | 2-0      | 2-1      | 2-2       \n",
     );
+
+    assert_eq!(table3.to_string(), expected);
 }
 
 #[test]
 fn table_join_horizontal_left() {
-    #[derive(Tabled)]
-    enum Domain {
-        Security,
-        Embeded,
-        Frontend,
-        Unknown,
-    }
+    let data1 = create_vector::<3, 3>();
+    let data2 = create_vector::<3, 3>();
 
-    #[derive(Tabled)]
-    struct Developer(#[header("name")] &'static str);
+    let table1 = Table::new(&data1)
+        .with(Style::PSQL)
+        .with(Modify::new(Full).with(Alignment::left()));
 
-    let data1 = vec![
-        (Developer("Terri Kshlerin"), Domain::Embeded),
-        (Developer("Catalina Dicki"), Domain::Security),
-        (Developer("Jennie Schmeler"), Domain::Frontend),
-        (Developer("Maxim Zhiburt"), Domain::Unknown),
-    ];
+    let table2 = Table::new(&data2)
+        .with(Style::ASCII)
+        .with(Modify::new(Full).with(Alignment::left()));
+        
+    let table3 = table2.with(join::Join::Horizontal(table1));
 
-    let table1 = Table::new(data1).with(Style::psql());
-
-    let data2 = vec![
-        (Developer("Taro Yamada"), Domain::Embeded),
-        (Developer("Hanako Sato"), Domain::Security),
-        (Developer("Ken Nakamura"), Domain::Unknown),
-        (Developer("Hiro Takahashi"), Domain::Frontend),
-    ];
-
-    let table2 = Table::new(data2).with(Style::psql());
-    let table3 = table1.with(join::Join::HorizontalLeft(table2));
-
-    assert_eq!(
-        table3.to_string(),
-        concat!(
-            "      name      | Security | Embeded | Frontend | Unknown       name       | Security | Embeded | Frontend | Unknown \n",
-            "----------------+----------+---------+----------+--------------------------+----------+---------+----------+---------\n",
-            "  Taro Yamada   |          |    +    |          |          Terri Kshlerin  |          |    +    |          |         \n",
-            "  Hanako Sato   |    +     |         |          |          Catalina Dicki  |    +     |         |          |         \n",
-            "  Ken Nakamura  |          |         |          |    +     Jennie Schmeler |          |         |    +     |         \n",
-            " Hiro Takahashi |          |         |    +     |           Maxim Zhiburt  |          |         |          |    +    \n",
-        )
+    let expected = concat!(
+        "+---+----------+----------+----------+                                    \n",
+        "| N | column 0 | column 1 | column 2 | N | column 0 | column 1 | column 2 \n",
+        "+---+----------+----------+----------+---+----------+----------+----------\n",
+        "| 0 | 0-0      | 0-1      | 0-2      | 0 | 0-0      | 0-1      | 0-2      \n",
+        "+---+----------+----------+----------+                                    \n",
+        "| 1 | 1-0      | 1-1      | 1-2      | 1 | 1-0      | 1-1      | 1-2      \n",
+        "+---+----------+----------+----------+                                    \n",
+        "| 2 | 2-0      | 2-1      | 2-2      | 2 | 2-0      | 2-1      | 2-2      \n",
+        "+---+----------+----------+----------+                                    \n",
     );
+
+    assert_eq!(table3.to_string(), expected);
 }
 
 
 #[test]
 fn table_join_horizontal_right() {
-    #[derive(Tabled)]
-    enum Domain {
-        Security,
-        Embeded,
-        Frontend,
-        Unknown,
-    }
+    let data1 = create_vector::<3, 3>();
+    let data2 = create_vector::<3, 3>();
 
-    #[derive(Tabled)]
-    struct Developer(#[header("name")] &'static str);
+    let table1 = Table::new(&data1)
+        .with(Style::PSQL)
+        .with(Modify::new(Full).with(Alignment::left()));
 
-    let data1 = vec![
-        (Developer("Terri Kshlerin"), Domain::Embeded),
-        (Developer("Catalina Dicki"), Domain::Security),
-        (Developer("Jennie Schmeler"), Domain::Frontend),
-        (Developer("Maxim Zhiburt"), Domain::Unknown),
-    ];
+    let table2 = Table::new(&data2)
+        .with(Style::ASCII)
+        .with(Modify::new(Full).with(Alignment::left()));
+        
+    let table3 = table1.with(join::Join::Horizontal(table2));
 
-    let table1 = Table::new(data1).with(Style::psql());
-
-    let data2 = vec![
-        (Developer("Taro Yamada"), Domain::Embeded),
-        (Developer("Hanako Sato"), Domain::Security),
-        (Developer("Ken Nakamura"), Domain::Unknown),
-        (Developer("Hiro Takahashi"), Domain::Frontend),
-    ];
-
-    let table2 = Table::new(data2).with(Style::psql());
-    let table3 = table1.with(join::Join::HorizontalRight(table2));
-
-    assert_eq!(
-        table3.to_string(),
-        concat!(
-            "      name       | Security | Embeded | Frontend | Unknown       name      | Security | Embeded | Frontend | Unknown \n",
-            "-----------------+----------+---------+----------+-------------------------+----------+---------+----------+---------\n",
-            " Terri Kshlerin  |          |    +    |          |           Taro Yamada   |          |    +    |          |         \n",
-            " Catalina Dicki  |    +     |         |          |           Hanako Sato   |    +     |         |          |         \n",
-            " Jennie Schmeler |          |         |    +     |           Ken Nakamura  |          |         |          |    +    \n",
-            "  Maxim Zhiburt  |          |         |          |    +     Hiro Takahashi |          |         |    +     |         \n",
-        )
+    let expected = concat!(
+        "                                    +---+----------+----------+----------+\n",
+        " N | column 0 | column 1 | column 2 | N | column 0 | column 1 | column 2 |\n",
+        "---+----------+----------+----------+---+----------+----------+----------+\n",
+        " 0 | 0-0      | 0-1      | 0-2      | 0 | 0-0      | 0-1      | 0-2      |\n",
+        "                                    +---+----------+----------+----------+\n",
+        " 1 | 1-0      | 1-1      | 1-2      | 1 | 1-0      | 1-1      | 1-2      |\n",
+        "                                    +---+----------+----------+----------+\n",
+        " 2 | 2-0      | 2-1      | 2-2      | 2 | 2-0      | 2-1      | 2-2      |\n",
+        "                                    +---+----------+----------+----------+\n",
     );
+
+    assert_eq!(table3.to_string(), expected);
 }
