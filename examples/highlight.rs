1a167a95020d309cb8835581a93dac09b660b15e
Maxim Zhiburt <zhiburt@gmail.com>
2021-12-30 22:05:53 +0300

    tabled/ Add highlight example
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ examples/highlight.rs
@@ -0,0 +1,16 @@
+//! The example can be run by this command
+//! `cargo run --example highlight`
+
+use tabled::{Border, Highlight, Style, TableIteratorExt};
+
+fn main() {
+    let data = vec![["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]];
+
+    let table = data.table().with(Style::PSEUDO).with(Highlight::cell(
+        2,
+        1,
+        Border::full('*', '*', '*', '*', '*', '*', '*', '*'),
+    ));
+
+    println!("{}", table);
+}
