4d6b82a68dbb4c56ada5f08206031dc9ff35a19a
Maxim Zhiburt <zhiburt@gmail.com>
2022-01-06 03:23:47 +0300

    tabled/ Update builder example
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/builder.rs
+++ examples/builder.rs
@@ -1,11 +1,26 @@
 //! The example can be run by this command
 //! `cargo run --example builder`
 
+use tabled::{Header, MaxWidth, Modify, Panel, Row, Style};
+
 fn main() {
-    let table = tabled::builder::Builder::default()
-        .set_header(["Name", "Issue"])
-        .add_row(["sadbuttrueasfuck", "Complex dynamic table creation"])
-        .build();
+    let message = r#"The terms "the ocean" or "the sea" used without specification refer to the interconnected body of salt water covering the majority of the Earth's surface"#;
+    let link = r#"https://en.wikipedia.org/wiki/Ocean"#;
+
+    let oceans = ["Atlantic", "Pacific", "Indian", "Southern", "Arctic"];
+
+    let mut builder = tabled::builder::Builder::default().set_header(["#", "Ocean"]);
+    for (i, ocean) in oceans.iter().enumerate() {
+        builder = builder.add_row([i.to_string(), ocean.to_string()]);
+    }
+
+    let table = builder
+        .build()
+        .with(Header(message))
+        .with(Header(link))
+        .with(Panel("=".repeat(link.len()), 2))
+        .with(Modify::new(Row(1..2)).with(MaxWidth::wrapping(link.len())))
+        .with(Style::GITHUB_MARKDOWN);
 
     println!("{}", table);
 }
