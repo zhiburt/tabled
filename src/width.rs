099a2b8440dce3f86dc78fb3876a0be28da0dec4
Maxim Zhiburt <zhiburt@gmail.com>
2022-02-02 02:29:29 +0300

    tabled/ Add keep_words option for MaxWidth::wrapping
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/width.rs
+++ src/width.rs
@@ -71,24 +71,36 @@ where
 
 pub struct Wrap {
     width: usize,
+    keep_words: bool,
 }
 
 impl Wrap {
     pub fn new(width: usize) -> Self {
-        Self { width }
+        Self {
+            width,
+            keep_words: false,
+        }
+    }
+
+    /// Set the wrap's keep words.
+    pub fn keep_words(mut self) -> Self {
+        self.keep_words = true;
+        self
     }
 }
 
 impl CellOption for Wrap {
     fn change_cell(&mut self, grid: &mut Grid, row: usize, column: usize) {
         let content = grid.get_cell_content(row, column);
-        let wrapped_content = split(content, self.width);
-        if wrapped_content.len() != content.len() {
-            grid.set(
-                &Entity::Cell(row, column),
-                Settings::new().text(wrapped_content),
-            )
-        }
+        let wrapped_content = if !self.keep_words {
+            split(content, self.width)
+        } else {
+            split_keeping_words(content, self.width)
+        };
+        grid.set(
+            &Entity::Cell(row, column),
+            Settings::new().text(wrapped_content),
+        )
     }
 }
 
@@ -128,6 +140,126 @@ pub(crate) fn split(s: &str, width: usize) -> String {
             chunks(s, width).join("\n")
         }
     }
+}
+
+#[cfg(not(feature = "color"))]
+fn split_keeping_words(s: &str, width: usize) -> String {
+    let mut buf = String::new();
+    let mut i = 0;
+    for c in s.chars() {
+        if i != 0 && i % width == 0 {
+            let prev_c = buf.chars().last().unwrap();
+            let is_splitting_word = !prev_c.is_whitespace() && !c.is_whitespace();
+            if is_splitting_word {
+                let pos = buf.chars().rev().position(|c| c.is_whitespace());
+                match pos {
+                    Some(pos) => {
+                        if pos < width {
+                            // it's a part of a word which we is ok to move to the next line;
+                            // we know that there will be enough space for this part + next character.
+                            //
+                            // todo: test about this next char space
+                            let range_len = buf
+                                .chars()
+                                .rev()
+                                .take(pos)
+                                .map(|c| c.len_utf8())
+                                .sum::<usize>();
+                            buf.insert(buf.len() - range_len, '\n');
+                            i = range_len;
+                        } else {
+                            // The words is too long to be moved,
+                            // we can't move it any way so just leave everything as it is
+                            buf.push('\n');
+                        }
+                    }
+                    None => {
+                        // We don't find a whitespace
+                        // so its a long word so we can do nothing about it
+                        buf.push('\n');
+                    }
+                }
+            } else {
+                // This place doesn't separate a word
+                // So we just do a general split.
+                buf.push('\n');
+            }
+        }
+
+        buf.push(c);
+
+        i += 1;
+    }
+
+    buf
+}
+
+#[cfg(feature = "color")]
+fn split_keeping_words(s: &str, width: usize) -> String {
+    use ansi_str::AnsiStr;
+
+    let mut buf = String::new();
+    let mut s = s.to_string();
+    while !s.is_empty() {
+        let width = to_byte_length(&s, width);
+        let (mut lhs, mut rhs) = s.ansi_split_at(width);
+
+        let lhs_stripped = lhs.ansi_strip();
+        let left_ends_with_letter = lhs_stripped
+            .chars()
+            .last()
+            .map_or(false, |c| !c.is_whitespace());
+        let right_starts_with_letter = rhs
+            .ansi_strip()
+            .chars()
+            .next()
+            .map_or(false, |c| !c.is_whitespace());
+        let is_splitting_word = left_ends_with_letter && right_starts_with_letter;
+        if is_splitting_word {
+            let pos = lhs_stripped.chars().rev().position(|c| c.is_whitespace());
+            match pos {
+                Some(pos) => {
+                    if pos < width {
+                        // it's a part of a word which we is ok to move to the next line;
+                        // we know that there will be enough space for this part + next character.
+                        //
+                        // todo: test about this next char space
+                        let range_len = lhs_stripped
+                            .chars()
+                            .rev()
+                            .take(pos)
+                            .map(|c| c.len_utf8())
+                            .sum::<usize>();
+
+                        let move_part = lhs.ansi_get(lhs_stripped.len() - range_len..).unwrap();
+                        lhs = lhs.ansi_get(..lhs_stripped.len() - range_len).unwrap();
+                        rhs = move_part + &rhs;
+
+                        buf.push_str(&lhs);
+                        buf.push('\n');
+                    } else {
+                        // The words is too long to be moved,
+                        // we can't move it any way so just leave everything as it is
+                        buf.push_str(&lhs);
+                        buf.push('\n');
+                    }
+                }
+                None => {
+                    // We don't find a whitespace
+                    // so its a long word so we can do nothing about it
+                    buf.push_str(&lhs);
+                    buf.push('\n');
+                }
+            }
+        } else {
+            buf.push_str(&lhs);
+            buf.push('\n');
+        }
+
+        s = rhs;
+    }
+
+    buf
 }
 
 #[cfg(feature = "color")]
