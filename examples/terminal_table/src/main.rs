93f3442ec740c39a2c2d3e443c92c11c0928fd2d
Maxim Zhiburt <zhiburt@gmail.com>
2022-03-09 01:44:04 +0300

    tabled/ Add terminal_table example
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ examples/terminal_table/src/main.rs
@@ -0,0 +1,45 @@
+//! The example can be run by this command
+//! `cargo run --example terminal_table`
+
+use tabled::{Alignment, Full, Modify, Style, TableIteratorExt, Tabled, TotalWidth};
+
+#[derive(Tabled)]
+struct Release {
+    version: &'static str,
+    published_date: &'static str,
+    is_active: bool,
+    major_feature: &'static str,
+}
+
+const DATA: [Release; 3] = [
+    Release {
+        version: "0.2.1",
+        published_date: "2021-06-23",
+        is_active: true,
+        major_feature: "#[header(inline)] attribute",
+    },
+    Release {
+        version: "0.2.0",
+        published_date: "2021-06-19",
+        is_active: false,
+        major_feature: "API changes",
+    },
+    Release {
+        version: "0.1.4",
+        published_date: "2021-06-07",
+        is_active: false,
+        major_feature: "display_with attribute",
+    },
+];
+
+fn main() {
+    let (terminal_size::Width(width), _) = terminal_size::terminal_size().unwrap();
+
+    let table = DATA
+        .table()
+        .with(Style::extended())
+        .with(Modify::new(Full).with(Alignment::center_horizontal()))
+        .with(TotalWidth::new(width as usize));
+
+    println!("{}", table);
+}
