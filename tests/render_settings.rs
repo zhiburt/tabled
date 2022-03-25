b461c3550b561b1b45595e26fa815b51ae7d21c5
Maxim Zhiburt <zhiburt@gmail.com>
2022-03-25 23:51:18 +0300

    tabled/ Add configuration for tab size
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- tests/render_settings.rs
+++ tests/render_settings.rs
@@ -1,8 +1,8 @@
 use crate::util::create_vector;
 use tabled::{
-    object::Full,
+    object::{Cell, Full},
     render_settings::{AlignmentStrategy, RenderSettings, TrimStrategy},
-    Alignment, Modify, Style, Table,
+    Alignment, Modify, Span, Style, Table,
 };
 
 mod util;
@@ -116,4 +116,99 @@ fn alignment_per_line_with_trim() {
     );
 
     assert_eq!(table, expected);
+}
+
+#[test]
+fn tab_size_test() {
+    let mut data = create_vector::<3, 3>();
+    data[1][0] = String::from("123\t123\tasdasd");
+    data[2][2] = String::from("htt\tps://\nwww\n.\nred\that\n.c\tom\n/en");
+
+    let mut table = Table::new(&data).with(Style::psql());
+
+    let expected = concat!(
+        "          N           | column 0 |   column 1   | column 2 \n",
+        "----------------------+----------+--------------+----------\n",
+        "          0           |   0-0    |     0-1      |   0-2    \n",
+        " 123    123    asdasd |   1-0    |     1-1      |   1-2    \n",
+        "          2           |   2-0    | htt    ps:// |   2-2    \n",
+        "                      |          | www          |          \n",
+        "                      |          | .            |          \n",
+        "                      |          | red    hat   |          \n",
+        "                      |          | .c    om     |          \n",
+        "                      |          | /en          |          \n",
+    );
+
+    assert_eq!(table.to_string(), expected);
+
+    table = table.with(
+        Modify::new(Full)
+            .with(Alignment::right())
+            .with(RenderSettings::default().set_tab_size(2)),
+    );
+
+    let expected = concat!(
+        "                N | column 0 |   column 1 | column 2 \n",
+        "------------------+----------+------------+----------\n",
+        "                0 |      0-0 |        0-1 |      0-2 \n",
+        " 123  123  asdasd |      1-0 |        1-1 |      1-2 \n",
+        "                2 |      2-0 | htt  ps:// |      2-2 \n",
+        "                  |          | www        |          \n",
+        "                  |          | .          |          \n",
+        "                  |          | red  hat   |          \n",
+        "                  |          | .c  om     |          \n",
+        "                  |          | /en        |          \n",
+    );
+
+    assert_eq!(table.to_string(), expected);
+
+    table = table.with(
+        Modify::new(Full)
+            .with(Alignment::right())
+            .with(RenderSettings::default().set_tab_size(0)),
+    );
+
+    let expected = concat!(
+        "            N | column 0 | column 1 | column 2 \n",
+        "--------------+----------+----------+----------\n",
+        "            0 |      0-0 |      0-1 |      0-2 \n",
+        " 123123asdasd |      1-0 |      1-1 |      1-2 \n",
+        "            2 |      2-0 | https:// |      2-2 \n",
+        "              |          | www      |          \n",
+        "              |          | .        |          \n",
+        "              |          | redhat   |          \n",
+        "              |          | .com     |          \n",
+        "              |          | /en      |          \n",
+    );
+
+    assert_eq!(table.to_string(), expected);
+}
+
+#[test]
+fn tab_size_span_test() {
+    let mut data = create_vector::<3, 3>();
+    data[0][0] = String::from("\tH\t\tello\tWorld");
+    data[1][0] = String::from("123\t123\tasdasd");
+    data[2][2] = String::from("htt\tps://\nwww\n.\nred\that\n.c\tom\n/en");
+
+    let table = Table::new(&data)
+        .with(Style::psql())
+        .with(Modify::new(Cell(0, 0)).with(Span::column(3)))
+        .with(Modify::new(Cell(1, 0)).with(Span::column(2)))
+        .with(Modify::new(Cell(2, 1)).with(Span::column(2)));
+
+    let expected = concat!(
+        "                     N                     | column 2 \n",
+        "----------------------+-----+--------------+----------\n",
+        "     H        ello    World |     0-1      |   0-2    \n",
+        " 123    123    asdasd |        1-0         |   1-2    \n",
+        "          2           | 2-0 | htt    ps:// |   2-2    \n",
+        "                      |     | www          |          \n",
+        "                      |     | .            |          \n",
+        "                      |     | red    hat   |          \n",
+        "                      |     | .c    om     |          \n",
+        "                      |     | /en          |          \n",
+    );
+
+    assert_eq!(table.to_string(), expected);
 }
