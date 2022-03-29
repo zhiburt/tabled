d18030586e2551c9c9f9bce02f9b0a2b33f6fad1
Maxim Zhiburt <zhiburt@gmail.com>
2022-03-29 19:06:34 +0300

    tabled/ Add more margin tests
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- tests/margin_test.rs
+++ tests/margin_test.rs
@@ -1,7 +1,7 @@
 use crate::util::create_vector;
 use tabled::{
     style::{Border, Style},
-    Cell, Highlight, Margin, Modify, Span, Table,
+    Cell, Highlight, Margin, MaxWidth, MinWidth, Modify, Span, Table,
 };
 
 mod util;
@@ -89,6 +89,60 @@ fn table_with_empty_margin() {
         "   |          |      redhat         \n",
         "   |          |      .com           \n",
         "   |          |      /en            \n",
+    );
+
+    println!("{}", table);
+
+    assert_eq!(table, expected);
+}
+
+#[test]
+fn table_with_margin_and_min_width() {
+    let data = create_vector::<3, 3>();
+
+    let table = Table::new(&data)
+        .with(Style::psql())
+        .with(Modify::new(Cell(1, 1)).with(Span::column(2)))
+        .with(Margin::new(1, 1, 1, 1).set_fill('>', '<', 'V', '^'))
+        .with(MaxWidth::truncating(20))
+        .to_string();
+
+    // todo: Seens like incorrect a bit
+    let expected = concat!(
+        "VVVVVVVVVVVVVVVVVVVV\n",
+        "> N | co | c |  c  <\n",
+        ">---+----+---+-----<\n",
+        "> 0 |  0-0   | 0-2 <\n",
+        "> 1 | 1- | 1 |  1  <\n",
+        "> 2 | 2- | 2 |  2  <\n",
+        "^^^^^^^^^^^^^^^^^^^^\n",
+    );
+
+    println!("{}", table);
+
+    assert_eq!(table, expected);
+}
+
+#[test]
+fn table_with_margin_and_max_width() {
+    let data = create_vector::<3, 3>();
+
+    let table = Table::new(&data)
+        .with(Style::psql())
+        .with(Modify::new(Cell(1, 1)).with(Span::column(2)))
+        .with(Margin::new(1, 1, 1, 1).set_fill('>', '<', 'V', '^'))
+        .with(MinWidth::new(50))
+        .to_string();
+
+    // todo: Seens like incorrect a bit
+    let expected = concat!(
+        "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV\n",
+        "> N    | column 0    | column 1    | column 2    <\n",
+        ">------+-------------+-------------+-------------<\n",
+        "> 0    |          0-0              |   0-2       <\n",
+        "> 1    |   1-0       |   1-1       |   1-2       <\n",
+        "> 2    |   2-0       |   2-1       |   2-2       <\n",
+        "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
     );
 
     println!("{}", table);
