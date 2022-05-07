8cbddf5c75b270edce0fa12abd1076556ed811c9
Maxim Zhiburt <zhiburt@gmail.com>
2022-05-07 19:05:23 +0300

    tabled/ Refactoring color.rs example
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- tests/style_test.rs
+++ tests/style_test.rs
@@ -1240,6 +1240,11 @@ fn border_colored_test() {
         )
         .to_string();
 
+    eprintln!("{:?}", "*".green().on_red().to_string());
+    eprintln!("{:?}", '*'.green().on_red().to_string());
+    eprintln!("{:?}", '*'.blue().to_string());
+    assert!(false);
+
     assert_eq!(
         table,
         concat!(
