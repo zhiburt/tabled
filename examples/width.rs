2c8035b64309cef21aa66cf95c65fb7a09a8bf9b
Maxim Zhiburt <zhiburt@gmail.com>
2022-05-31 20:17:22 +0300

    tabled/ Update width.rs example
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/width.rs
+++ examples/width.rs
@@ -1,24 +1,37 @@
 //! The example can be run by this command
 //! `cargo run --example width`
 
-use tabled::{object::Segment, Alignment, MaxWidth, Modify, Style, Table};
+use tabled::{
+    object::Segment,
+    width::{MinWidth, Percent},
+    Alignment, MaxWidth, Modify, Style, Table,
+};
 
 fn main() {
     let data = [
-        ["Hello World", "123123123231"],
-        ["Hello World", "zxczczxcxczxczxc"],
-        ["Hello World", "[[[[[[[[[[[[[[[[["],
+        ["Hello World!!!", "3.3.22.2"],
+        ["Guten Morgen", "1.1.1.1"],
+        ["Добры вечар", "127.0.0.1"],
+        ["Bonjour le monde", ""],
+        ["Ciao mondo", ""],
     ];
 
-    let table = Table::new(&data).with(Style::github_markdown()).with(
-        Modify::new(Segment::all())
-            .with(MaxWidth::truncating(10).suffix("..."))
-            .with(Alignment::left()),
-    );
+    let table = Table::builder(data)
+        .build()
+        .with(Style::github_markdown())
+        .with(Modify::new(Segment::all()).with(Alignment::left()));
 
-    println!("{}", table);
+    println!("Original table\n{}", table);
+
+    let table = table.with(MaxWidth::truncating(20).suffix("..."));
+
+    println!("Truncated table\n{}", table);
 
     let table = table.with(Modify::new(Segment::all()).with(MaxWidth::wrapping(5)));
 
-    println!("{}", table);
+    println!("Wrapped table\n{}", table);
+
+    let table = table.with(MinWidth::new(Percent(200)));
+
+    println!("Widen table\n{}", table);
 }
