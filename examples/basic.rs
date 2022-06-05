ef0ff8fa9948893dac49deb5b65667244fc02fe2
Maxim Zhiburt <zhiburt@gmail.com>
2022-06-06 01:58:55 +0300

    tabled/ Revert accidental changes
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,33 +1,52 @@
-use std::iter::FromIterator;
+//! The example can be run by this command
+//! `cargo run --example basic`
 
-use tabled::{
-    builder::Builder,
-    object::{Cell, Object},
-    Modify, Padding, Span,
-};
+use tabled::{object::Rows, Alignment, Modify, Style, Table, Tabled};
+
+#[derive(Tabled)]
+struct Distribution {
+    name: &'static str,
+    #[tabled(display_with = "Self::display_based_on")]
+    based_on: Option<&'static str>,
+    is_active: bool,
+    is_cool: bool,
+}
+
+impl Distribution {
+    fn display_based_on(o: &Option<&'static str>) -> String {
+        match o {
+            &Some(s) => s.into(),
+            None => "Independent".into(),
+        }
+    }
+}
 
 fn main() {
     let data = [
-        ["Version", "IHL", "TOS", "Total length", ""],
-        ["Identification", "", "", "Flags", "Frgment offset"],
-        ["TTL", "", "Protocol", "Header checksum", ""],
-        ["Source address", "", "", "", ""],
-        ["Destination address", "", "", "", ""],
-        ["Options", "", "", "", ""],
-        ["Data", "", "", "", ""],
+        Distribution {
+            name: "Manjaro",
+            based_on: Some("Arch"),
+            is_cool: true,
+            is_active: true,
+        },
+        Distribution {
+            name: "Debian",
+            based_on: None,
+            is_cool: true,
+            is_active: true,
+        },
+        Distribution {
+            name: "Debian",
+            based_on: None,
+            is_cool: true,
+            is_active: true,
+        },
     ];
 
-    let table = Builder::from_iter(data)
-        .build()
-        .with(Modify::new(Cell(0, 3)).with(Span::column(2)))
-        .with(Modify::new(Cell(1, 0)).with(Span::column(3)))
-        .with(Modify::new(Cell(2, 0)).with(Span::column(2)))
-        .with(Modify::new(Cell(2, 3)).with(Span::column(2)))
-        .with(Modify::new(Cell(3, 0)).with(Span::column(5)))
-        .with(Modify::new(Cell(4, 0)).with(Span::column(5)))
-        .with(Modify::new(Cell(5, 0)).with(Span::column(5)))
-        .with(Modify::new(Cell(6, 0)).with(Span::column(5)))
-        .with(Modify::new(Cell(5, 0).and(Cell(6, 0))).with(Padding::new(1, 1, 1, 1)));
+    let table = Table::new(&data)
+        .with(Style::modern())
+        .with(Modify::new(Rows::first()).with(Alignment::center()))
+        .with(Modify::new(Rows::new(1..)).with(Alignment::left()));
 
     println!("{}", table);
 }
