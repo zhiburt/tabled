49e4be48e55172433336529a4391d58d3c712bc8
Maxim Zhiburt <zhiburt@gmail.com>
2022-06-10 00:54:33 +0300

    tabled/ Add ModifyObject trait
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/table.rs
+++ src/table.rs
@@ -8,7 +8,7 @@ use std::{fmt, iter::FromIterator};
 
 use papergrid::Grid;
 
-use crate::{builder::Builder, object::Object, Tabled};
+use crate::{builder::Builder, Tabled};
 
 /// A trait which is responsilbe for configuration of a [Table].
 pub trait TableOption {
@@ -163,100 +163,6 @@ where
         T: IntoIterator<Item = D>,
     {
         Self::builder(iter).build()
-    }
-}
-
-/// Modify structure provide an abstraction, to be able to apply
-/// a set of [CellOption]s to the same object.
-///
-/// Be aware that the settings are applied all to a cell at a time.
-/// So sometimes you may need to make a several calls of [Modify] in order to achieve the desired affect.
-pub struct Modify<O> {
-    obj: O,
-}
-
-impl<O> Modify<O>
-where
-    O: Object,
-{
-    /// Creates a new [Modify] without any options.
-    pub fn new(obj: O) -> Self {
-        Self { obj }
-    }
-
-    /// It's a generic function which stores a [CellOption].
-    ///
-    /// IMPORTANT:
-    ///     The function *doesn't* changes a [Table].
-    ///     [Table] will be changed only after passing [Modify] object to [Table::with].
-    pub fn with<F>(self, s: F) -> ModifyList<O, F>
-    where
-        F: CellOption,
-    {
-        ModifyList {
-            obj: self.obj,
-            modifiers: s,
-        }
-    }
-}
-
-/// ModifyList is a container of [CellOption]s which are applied to a set [Object].
-pub struct ModifyList<O, S> {
-    obj: O,
-    modifiers: S,
-}
-
-impl<O, S> ModifyList<O, S>
-where
-    O: Object,
-    S: CellOption,
-{
-    /// With a generic function which stores a [CellOption].
-    ///
-    /// IMPORTANT:
-    ///     The function *doesn't* changes a [Table].
-    ///     [Table] will be changed only after passing [Modify] object to [Table::with].
-    pub fn with<F>(self, s: F) -> ModifyList<O, CellSettingsList<S, F>>
-    where
-        F: CellOption,
-    {
-        ModifyList {
-            obj: self.obj,
-            modifiers: CellSettingsList {
-                s1: self.modifiers,
-                s2: s,
-            },
-        }
-    }
-}
-
-impl<O, S> TableOption for ModifyList<O, S>
-where
-    O: Object,
-    S: CellOption,
-{
-    fn change(&mut self, grid: &mut Grid) {
-        let cells = self.obj.cells(grid.count_rows(), grid.count_columns());
-        for (row, column) in cells {
-            self.modifiers.change_cell(grid, row, column)
-        }
-    }
-}
-
-/// CellSettingsList is a container of [CellOption]s.
-pub struct CellSettingsList<S1, S2> {
-    s1: S1,
-    s2: S2,
-}
-
-impl<S1, S2> CellOption for CellSettingsList<S1, S2>
-where
-    S1: CellOption,
-    S2: CellOption,
-{
-    fn change_cell(&mut self, grid: &mut Grid, row: usize, column: usize) {
-        self.s1.change_cell(grid, row, column);
-        self.s2.change_cell(grid, row, column);
     }
 }
 
