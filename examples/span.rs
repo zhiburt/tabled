28a72e0f0db63b8e91ae7d1fe86a139cdc853810
Maxim Zhiburt <zhiburt@gmail.com>
2022-08-19 13:06:11 +0300

    tabled/ Impl CellOption for String
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/span.rs
+++ examples/span.rs
@@ -11,13 +11,13 @@ fn main() {
 
     let table = data
         .table()
-        .with(h_span(0, 0, 5).with(|_: &str| "span all 5 columns".to_string()))
-        .with(h_span(1, 0, 4).with(|_: &str| "span 4 columns".to_string()))
-        .with(h_span(2, 0, 2).with(|_: &str| "span 2 columns".to_string()))
-        .with(v_span(2, 4, 4).with(|_: &str| "just 1 column\nspan\n4\ncolumns".to_string()))
-        .with(v_span(3, 1, 2).with(|_: &str| "span 2 columns\nspan\n2\ncolumns".to_string()))
+        .with(h_span(0, 0, 5).with(String::from("span all 5 columns")))
+        .with(h_span(1, 0, 4).with(String::from("span 4 columns")))
+        .with(h_span(2, 0, 2).with(String::from("span 2 columns")))
+        .with(v_span(2, 4, 4).with(String::from("just 1 column\nspan\n4\ncolumns")))
+        .with(v_span(3, 1, 2).with(String::from("span 2 columns\nspan\n2\ncolumns")))
+        .with(v_span(2, 3, 3).with(String::from("just 1 column\nspan\n3\ncolumns")))
         .with(h_span(3, 1, 2))
-        .with(v_span(2, 3, 3).with(|_: &str| "just 1 column\nspan\n3\ncolumns".to_string()))
         .with(Style::modern())
         .with(Style::correct_spans())
         .with(Segment::all().modify().with(Alignment::center_vertical()));
