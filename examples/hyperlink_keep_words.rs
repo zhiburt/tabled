229372fb290c5ecf08d7fa4394ecd3dacb14d796
Dan Davison <dandavison7@gmail.com>
2022-09-13 18:57:02 -0400

    Add hyperlink example

---
+++ examples/hyperlink_keep_words.rs
@@ -0,0 +1,55 @@
+//! The example can be run by this command
+//! `cargo run --example hyperlink`
+
+use std::iter::FromIterator;
+
+use tabled::{object::Segment, Alignment, ModifyObject, Style, Table, Tabled, Width};
+
+#[derive(Tabled)]
+struct Distribution {
+    name: String,
+    is_hyperlink: bool,
+}
+
+fn format_osc8_hyperlink(url: &str, text: &str) -> String {
+    format!(
+        "{osc}8;;{url}{st}{text}{osc}8;;{st}",
+        url = url,
+        text = text,
+        osc = "\x1b]",
+        st = "\x1b\\"
+    )
+}
+
+fn main() {
+    let data = [
+        Distribution {
+            name: format_osc8_hyperlink("https://www.debian.org/", "Debian"),
+            is_hyperlink: true,
+        },
+        Distribution {
+            name: "Debian".into(),
+            is_hyperlink: false,
+        },
+        Distribution {
+            name: format_osc8_hyperlink(
+                "https://www.debian.org/",
+                "DebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebian",
+            ),
+            is_hyperlink: true,
+        },
+        Distribution {
+            name: "DebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebianDebian".into(),
+            is_hyperlink: false,
+        },
+    ];
+
+    let table = Table::from_iter(&data).with(Style::ascii_rounded()).with(
+        Segment::all()
+            .modify()
+            .with(Width::wrap(30).keep_words())
+            .with(Alignment::left()),
+    );
+
+    println!("{}", table);
+}
