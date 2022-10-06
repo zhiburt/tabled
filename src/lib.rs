d67580961d2fbc9ae2be0b8564cd9ec6cf713eb7
Maxim Zhiburt <zhiburt@gmail.com>
2022-10-06 10:17:03 +0300

    tabled/ Fix cargo fmt
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -284,31 +284,31 @@ pub use crate::{
 pub use crate::features::{color, highlight, margin_color, padding_color};
 
 /// A derive to implement a [`Tabled`] trait.
-/// 
+///
 /// The macros available only when `derive` feature in turned on (and it is by default).
-/// 
+///
 /// To be able to use the derive each field must implement `std::fmt::Display`.
 /// The following example will cause a error because of that.
-/// 
+///
 /// ```rust,compile_fail
 /// use tabled::Tabled;
 /// #[derive(Tabled)]
 /// struct SomeType {
 ///     field1: SomeOtherType,
 /// }
-/// 
+///
 /// struct SomeOtherType;
 /// ```
-/// 
+///
 /// Bellow you'll find available options for it.
-/// 
+///
 /// ### Override a column name
-/// 
+///
 /// You can use a `#[tabled(rename = "")]` attribute to override a column name.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// struct Person {
 ///     #[tabled(rename = "Name")]
@@ -317,16 +317,16 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///     last_name: &'static str,
 /// }
 /// ```
-/// 
+///
 /// ### Hide a column
-/// 
+///
 /// You can mark filds as hidden in which case they fill be ignored and not be present on a sheet.
-/// 
+///
 /// A similar affect could be achieved by the means of a `Disable` setting.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// struct Person {
 ///    id: u8,
@@ -335,14 +335,14 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///    name: &'static str,
 /// }
 /// ```
-/// 
+///
 /// ### Set column order
-/// 
+///
 /// You can change the order in which they will be displayed in table.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// struct Person {
 ///    id: u8,
@@ -352,27 +352,27 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///    name: &'static str,
 /// }
 /// ```
-/// 
+///
 /// ### Format fields
-/// 
+///
 /// As was said already, using `#[derive(Tabled)]` is possible only when all fields implement a `Display` trait.
 /// However, this may be often not the case for example when a field uses the `Option` type. There's 2 common ways how to solve this:
-/// 
+///
 /// - Implement `Tabled` trait manually for a type.
 /// - Wrap `Option` to something like `DisplayedOption<T>(Option<T>)` and implement a Display trait for it.
-/// 
+///
 /// Alternatively, you can use the `#[tabled(display_with = "func")]` attribute for the field to specify a display function.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// pub struct MyRecord {
 ///     pub id: i64,
 ///     #[tabled(display_with = "display_option")]
 ///     pub valid: Option<bool>
 /// }
-/// 
+///
 /// fn display_option(o: &Option<bool>) -> String {
 ///     match o {
 ///         Some(s) => format!("is valid thing = {}", s),
@@ -380,20 +380,20 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///     }
 /// }
 /// ```
-/// 
+///
 /// It's also possible to change function argument to be `&self`,
 /// using `#[tabled(display_with("some_function", args))]`
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// pub struct MyRecord {
 ///     pub id: i64,
 ///     #[tabled(display_with("Self::display_valid", args))]
 ///     pub valid: Option<bool>
 /// }
-/// 
+///
 /// impl MyRecord {
 ///     fn display_valid(&self) -> String {
 ///         match self.valid {
@@ -403,15 +403,15 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///     }
 /// }
 /// ```
-/// 
+///
 /// ### Format headers
-/// 
+///
 /// Beside `#[tabled(rename = "")]` you can change a format of a column name using
 /// `#[tabled(rename_all = "UPPERCASE")]`.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// #[tabled(rename_all = "CamelCase")]
 /// struct Person {
@@ -422,15 +422,15 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///     middle_name: &'static str,
 /// }
 /// ```
-/// 
+///
 /// ### Inline
-/// 
+///
 /// It's possible to inline internal data if it implements the `Tabled` trait using `#[tabled(inline)]`.
 /// You can also set a prefix which will be used for all inlined elements by `#[tabled(inline("prefix>>"))]`.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// struct Person {
 ///     id: u8,
@@ -438,19 +438,19 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///     #[tabled(inline)]
 ///     ed: Education,
 /// }
-/// 
+///
 /// #[derive(Tabled)]
 /// struct Education {
 ///     uni: &'static str,
 ///     graduated: bool,
 /// }
 /// ```
-/// 
+///
 /// And it works for enums as well.
-/// 
-/// ```rust
+///
+/// ```rust,no_run
 /// use tabled::Tabled;
-/// 
+///
 /// #[derive(Tabled)]
 /// enum Vehicle {
 ///     #[tabled(inline("Auto::"))]
@@ -464,7 +464,7 @@ pub use crate::features::{color, highlight, margin_color, padding_color};
 ///         #[tabled(inline)] Bike,
 ///     ),
 /// }
-/// 
+///
 /// #[derive(Tabled)]
 /// struct Bike {
 ///     brand: &'static str,
