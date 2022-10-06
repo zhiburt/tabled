fa6b4c584ca81d8f8c3924fff969764e25c22395
Maxim Zhiburt <zhiburt@gmail.com>
2022-10-06 09:40:32 +0300

    Create a short version of README.md for docs.rs
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ docs.rs.md
@@ -0,0 +1,58 @@
+[<img alt="github" src="https://img.shields.io/badge/github-zhiburt/tabled-8da0cb?style=for-the-badge&labelColor=555555&logo=github" height="20">](https://github.com/zhiburt/tabled/)
+[<img alt="crates.io" src="https://img.shields.io/crates/v/tabled.svg?style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/tabled)
+[<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-tabled-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs" height="20">](https://docs.rs/tabled)
+[<img alt="build status" src="https://img.shields.io/github/workflow/status/zhiburt/tabled/Continuous%20integration/master?style=for-the-badge" height="20">](https://github.com/zhiburt/tabled/actions)
+[<img alt="coverage" src="https://img.shields.io/coveralls/github/zhiburt/tabled/master?style=for-the-badge" height="20">](https://coveralls.io/github/zhiburt/tabled)
+[<img alt="dependency status" src="https://deps.rs/repo/github/zhiburt/tabled/status.svg?style=for-the-badge" height="20">](https://deps.rs/repo/github/zhiburt/tabled)
+
+# tabled
+
+An easy to use library for pretty printing tables of Rust `struct`s and `enum`s.
+
+There are more examples and you can find in this [`README`](https://github.com/zhiburt/tabled/blob/master/README.md).
+
+## Usage
+
+To print a list of structs or enums as a table your types should implement the the `Tabled` trait or derive it with a `#[derive(Tabled)]` macro.
+Most of the default types implement the trait out of the box.
+
+```rust
+use tabled::{Table, Tabled};
+
+#[derive(Tabled)]
+struct Language {
+    name: String,
+    designed_by: String,
+    invented_year: usize,
+}
+
+impl Language {
+    fn new(name: &str, designed_by: &str, invented_year: usize) -> Self {
+        Self {
+            name: name.to_string(),
+            designed_by: designed_by.to_string(),
+            invented_year,
+        }
+    }
+}
+
+let languages = vec![
+    Language::new("C", "Dennis Ritchie", 1972),
+    Language::new("Go", "Rob Pike", 2009),
+    Language::new("Rust", "Graydon Hoare", 2010),
+];
+
+let table = Table::new(languages).to_string();
+
+let expected = "+------+----------------+---------------+\n\
+                | name | designed_by    | invented_year |\n\
+                +------+----------------+---------------+\n\
+                | C    | Dennis Ritchie | 1972          |\n\
+                +------+----------------+---------------+\n\
+                | Go   | Rob Pike       | 2009          |\n\
+                +------+----------------+---------------+\n\
+                | Rust | Graydon Hoare  | 2010          |\n\
+                +------+----------------+---------------+";
+
+assert_eq!(table, expected);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -283,6 +283,195 @@ pub use crate::{
 #[cfg_attr(docsrs, doc(cfg(feature = "color")))]
 pub use crate::features::{color, highlight, margin_color, padding_color};
 
+/// A derive to implement a [`Tabled`] trait.
+/// 
+/// The macros available only when `derive` feature in turned on (and it is by default).
+/// 
+/// To be able to use the derive each field must implement `std::fmt::Display`.
+/// The following example will cause a error because of that.
+/// 
+/// ```rust,compile_fail
+/// use tabled::Tabled;
+/// #[derive(Tabled)]
+/// struct SomeType {
+///     field1: SomeOtherType,
+/// }
+/// 
+/// struct SomeOtherType;
+/// ```
+/// 
+/// Bellow you'll find available options for it.
+/// 
+/// ### Override a column name
+/// 
+/// You can use a `#[tabled(rename = "")]` attribute to override a column name.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// struct Person {
+///     #[tabled(rename = "Name")]
+///     first_name: &'static str,
+///     #[tabled(rename = "Surname")]
+///     last_name: &'static str,
+/// }
+/// ```
+/// 
+/// ### Hide a column
+/// 
+/// You can mark filds as hidden in which case they fill be ignored and not be present on a sheet.
+/// 
+/// A similar affect could be achieved by the means of a `Disable` setting.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// struct Person {
+///    id: u8,
+///    #[tabled(skip)]
+///    number: &'static str,
+///    name: &'static str,
+/// }
+/// ```
+/// 
+/// ### Set column order
+/// 
+/// You can change the order in which they will be displayed in table.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// struct Person {
+///    id: u8,
+///    #[tabled(order = 0)]
+///    number: &'static str,
+///    #[tabled(order = 1)]
+///    name: &'static str,
+/// }
+/// ```
+/// 
+/// ### Format fields
+/// 
+/// As was said already, using `#[derive(Tabled)]` is possible only when all fields implement a `Display` trait.
+/// However, this may be often not the case for example when a field uses the `Option` type. There's 2 common ways how to solve this:
+/// 
+/// - Implement `Tabled` trait manually for a type.
+/// - Wrap `Option` to something like `DisplayedOption<T>(Option<T>)` and implement a Display trait for it.
+/// 
+/// Alternatively, you can use the `#[tabled(display_with = "func")]` attribute for the field to specify a display function.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// pub struct MyRecord {
+///     pub id: i64,
+///     #[tabled(display_with = "display_option")]
+///     pub valid: Option<bool>
+/// }
+/// 
+/// fn display_option(o: &Option<bool>) -> String {
+///     match o {
+///         Some(s) => format!("is valid thing = {}", s),
+///         None => format!("is not valid"),
+///     }
+/// }
+/// ```
+/// 
+/// It's also possible to change function argument to be `&self`,
+/// using `#[tabled(display_with("some_function", args))]`
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// pub struct MyRecord {
+///     pub id: i64,
+///     #[tabled(display_with("Self::display_valid", args))]
+///     pub valid: Option<bool>
+/// }
+/// 
+/// impl MyRecord {
+///     fn display_valid(&self) -> String {
+///         match self.valid {
+///             Some(s) => format!("is valid thing = {}", s),
+///             None => format!("is not valid"),
+///         }
+///     }
+/// }
+/// ```
+/// 
+/// ### Format headers
+/// 
+/// Beside `#[tabled(rename = "")]` you can change a format of a column name using
+/// `#[tabled(rename_all = "UPPERCASE")]`.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// #[tabled(rename_all = "CamelCase")]
+/// struct Person {
+///     id: u8,
+///     number: &'static str,
+///     name: &'static str,
+///     #[tabled(rename_all = "snake_case")]
+///     middle_name: &'static str,
+/// }
+/// ```
+/// 
+/// ### Inline
+/// 
+/// It's possible to inline internal data if it implements the `Tabled` trait using `#[tabled(inline)]`.
+/// You can also set a prefix which will be used for all inlined elements by `#[tabled(inline("prefix>>"))]`.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// struct Person {
+///     id: u8,
+///     name: &'static str,
+///     #[tabled(inline)]
+///     ed: Education,
+/// }
+/// 
+/// #[derive(Tabled)]
+/// struct Education {
+///     uni: &'static str,
+///     graduated: bool,
+/// }
+/// ```
+/// 
+/// And it works for enums as well.
+/// 
+/// ```rust
+/// use tabled::Tabled;
+/// 
+/// #[derive(Tabled)]
+/// enum Vehicle {
+///     #[tabled(inline("Auto::"))]
+///     Auto {
+///         model: &'static str,
+///         engine: &'static str,
+///     },
+///     #[tabled(inline)]
+///     Bikecycle(
+///         &'static str,
+///         #[tabled(inline)] Bike,
+///     ),
+/// }
+/// 
+/// #[derive(Tabled)]
+/// struct Bike {
+///     brand: &'static str,
+///     price: f32,
+/// }
+/// ```
+// @todo: Move the comment to tabled_derive
 #[cfg(feature = "derive")]
 #[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub use tabled_derive::Tabled;
