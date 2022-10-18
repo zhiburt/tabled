696eec81380090256408ca53194ab39733dee814
Maxim Zhiburt <zhiburt@gmail.com>
2022-10-18 11:55:28 +0300

    Bump ansi-str (#236)
    * tabled/ Bump ansi-str
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>
    
    * json_to_table/ Fix version
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>
    
    * json_to_table/ Fix build
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>
    
    * tabled/ Add benchmark for local string functions
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>
    
    * papergrid/ Bump ansi-str
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>
    
    * fix Cargo.toml
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/features/width/wrap.rs
+++ src/features/width/wrap.rs
@@ -173,7 +173,6 @@ fn wrap_total_width<R, P>(
     decrease_widths(&mut widths, &min_widths, total_width, width, priority);
 
     let points = get_decrease_cell_list(cfg, &widths, &min_widths, (count_rows, count_cols));
-
     let mut wrap = Wrap::new(0);
     wrap.keep_words = keep_words;
     for ((row, col), width) in points {
@@ -201,11 +200,13 @@ pub(crate) fn wrap_text(text: &str, width: usize, keep_words: bool) -> String {
 
 #[cfg(feature = "color")]
 pub(crate) fn wrap_text(text: &str, width: usize, keep_words: bool) -> String {
+    use papergrid::util::strip_osc;
+
     if width == 0 {
         return String::new();
     }
 
-    let (text, url): (String, Option<String>) = link_extraction::strip_osc(text);
+    let (text, url): (String, Option<String>) = strip_osc(text);
     let (prefix, suffix) = build_link_prefix_suffix(url);
 
     if keep_words {
@@ -217,15 +218,16 @@ pub(crate) fn wrap_text(text: &str, width: usize, keep_words: bool) -> String {
 
 #[cfg(feature = "color")]
 fn build_link_prefix_suffix(url: Option<String>) -> (String, String) {
-    let (prefix, suffix) = if let Some(url) = url {
-        // https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
-        let osc8 = "\x1b]8;;";
-        let st = "\x1b\\";
-        (format!("{osc8}{url}{st}"), format!("{osc8}{st}"))
-    } else {
-        ("".to_string(), "".to_string())
-    };
-    (prefix, suffix)
+    match url {
+        Some(url) => {
+            // https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
+            let osc8 = "\x1b]8;;";
+            let st = "\x1b\\";
+
+            (format!("{}{}{}", osc8, url, st), format!("{}{}", osc8, st))
+        }
+        None => ("".to_string(), "".to_string()),
+    }
 }
 
 #[cfg(not(feature = "color"))]
@@ -562,242 +564,6 @@ fn split_string_at(text: &str, at: usize) -> (&str, &str, (usize, usize)) {
     let (lhs, rhs) = text.split_at(length);
 
     (lhs, rhs, (count_unknowns, split_char_size))
-}
-
-#[cfg(feature = "color")]
-mod link_extraction {
-    //! The module is based on Dan Davison <https://github.com/dandavison> delta <https://github.com/dandavison/delta> ansi library.
-
-    use core::str::Bytes;
-    use vte::Params;
-
-    /// Strip OSC codes from `s`. If `s` is a single OSC8 hyperlink, with no other text, then return
-    /// (s_with_all_hyperlinks_removed, Some(url)). If `s` does not meet this description, then return
-    /// (s_with_all_hyperlinks_removed, None). Any ANSI color sequences in `s` will be retained. See
-    /// https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
-    pub(super) fn strip_osc(s: &str) -> (String, Option<String>) {
-        #[derive(Debug)]
-        enum ExtractOsc8HyperlinkState {
-            ExpectOsc8Url,
-            ExpectFirstText,
-            ExpectMoreTextOrTerminator,
-            SeenOneHyperlink,
-            WillNotReturnUrl,
-        }
-
-        use ExtractOsc8HyperlinkState::*;
-
-        let mut url = None;
-        let mut state = ExpectOsc8Url;
-        let mut text = String::with_capacity(s.len());
-
-        for el in AnsiElementIterator::new(s) {
-            match el {
-                Element::Osc(i, j) => match state {
-                    ExpectOsc8Url => {
-                        url = Some(&s[i..j]);
-                        state = ExpectFirstText;
-                    }
-                    ExpectMoreTextOrTerminator => state = SeenOneHyperlink,
-                    _ => state = WillNotReturnUrl,
-                },
-                Element::Sgr(i, j) => text.push_str(&s[i..j]),
-                Element::Csi(i, j) => text.push_str(&s[i..j]),
-                Element::Esc(_, _) => {}
-                Element::Text(i, j) => {
-                    text.push_str(&s[i..j]);
-                    match state {
-                        ExpectFirstText => state = ExpectMoreTextOrTerminator,
-                        ExpectMoreTextOrTerminator => {}
-                        _ => state = WillNotReturnUrl,
-                    }
-                }
-            }
-        }
-
-        match state {
-            WillNotReturnUrl => (text, None),
-            _ => {
-                let url = url.and_then(|s| {
-                    s.strip_prefix("\x1b]8;;")
-                        .and_then(|s| s.strip_suffix('\x1b'))
-                });
-                if let Some(url) = url {
-                    (text, Some(url.to_string()))
-                } else {
-                    (text, None)
-                }
-            }
-        }
-    }
-
-    struct AnsiElementIterator<'a> {
-        // The input bytes
-        bytes: Bytes<'a>,
-
-        // The state machine
-        machine: vte::Parser,
-
-        // Becomes non-None when the parser finishes parsing an ANSI sequence.
-        // This is never Element::Text.
-        element: Option<Element>,
-
-        // Number of text bytes seen since the last element was emitted.
-        text_length: usize,
-
-        // Byte offset of start of current element.
-        start: usize,
-
-        // Byte offset of most rightward byte processed so far
-        pos: usize,
-    }
-
-    #[derive(Default)]
-    struct Performer {
-        // Becomes non-None when the parser finishes parsing an ANSI sequence.
-        // This is never Element::Text.
-        element: Option<Element>,
-
-        // Number of text bytes seen since the last element was emitted.
-        text_length: usize,
-    }
-
-    #[derive(Debug)]
-    enum Element {
-        // TODO: capture SGR Params. Delta captures these as an ansi_term::Style struct.
-        // https://github.com/dandavison/delta/blob/1193d54d5c90ab3a45048de3fd1e95c7c2580014/src/ansi/iterator.rs#L136-L137
-        // However, the ansi_term crate is unmaintained.
-        Sgr(usize, usize),
-        Csi(usize, usize),
-        Esc(usize, usize),
-        Osc(usize, usize),
-        Text(usize, usize),
-    }
-
-    impl Element {
-        fn set_range(&mut self, start: usize, end: usize) {
-            let (from, to) = match self {
-                Element::Sgr(from, to) => (from, to),
-                Element::Csi(from, to) => (from, to),
-                Element::Esc(from, to) => (from, to),
-                Element::Osc(from, to) => (from, to),
-                Element::Text(from, to) => (from, to),
-            };
-
-            *from = start;
-            *to = end;
-        }
-    }
-
-    impl<'a> AnsiElementIterator<'a> {
-        fn new(s: &'a str) -> Self {
-            Self {
-                machine: vte::Parser::new(),
-                bytes: s.bytes(),
-                element: None,
-                text_length: 0,
-                start: 0,
-                pos: 0,
-            }
-        }
-
-        fn advance_vte(&mut self, byte: u8) {
-            let mut performer = Performer::default();
-            self.machine.advance(&mut performer, byte);
-            self.element = performer.element;
-            self.text_length += performer.text_length;
-            self.pos += 1;
-        }
-    }
-
-    impl<'a> Iterator for AnsiElementIterator<'a> {
-        type Item = Element;
-
-        fn next(&mut self) -> Option<Element> {
-            // If the last element emitted was text, then there may be a non-text element waiting
-            // to be emitted. In that case we do not consume a new byte.
-            while self.element.is_none() {
-                match self.bytes.next() {
-                    Some(b) => self.advance_vte(b),
-                    None => break,
-                }
-            }
-
-            if let Some(mut element) = self.element.take() {
-                // There is a non-text element waiting to be emitted, but it may have preceding
-                // text, which must be emitted first.
-                if self.text_length > 0 {
-                    let start = self.start;
-                    self.start += self.text_length;
-                    self.text_length = 0;
-                    self.element = Some(element);
-                    return Some(Element::Text(start, self.start));
-                }
-
-                let start = self.start;
-                self.start = self.pos;
-                element.set_range(start, self.pos);
-
-                return Some(element);
-            }
-
-            if self.text_length > 0 {
-                self.text_length = 0;
-                return Some(Element::Text(self.start, self.pos));
-            }
-
-            None
-        }
-    }
-
-    // Based on https://github.com/alacritty/vte/blob/v0.9.0/examples/parselog.rs
-    impl vte::Perform for Performer {
-        fn csi_dispatch(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
-            if ignore || intermediates.len() > 1 {
-                return;
-            }
-
-            let is_sgr = c == 'm' && intermediates.first().is_none();
-            let element = if is_sgr {
-                if params.is_empty() {
-                    // Attr::Reset
-                    // Probably doesn't need to be handled: https://github.com/dandavison/delta/pull/431#discussion_r536883568
-                    None
-                } else {
-                    Some(Element::Sgr(0, 0))
-                }
-            } else {
-                Some(Element::Csi(0, 0))
-            };
-
-            self.element = element;
-        }
-
-        fn print(&mut self, c: char) {
-            self.text_length += c.len_utf8();
-        }
-
-        fn execute(&mut self, byte: u8) {
-            // E.g. '\n'
-            if byte < 128 {
-                self.text_length += 1;
-            }
-        }
-
-        fn hook(&mut self, _params: &Params, _intermediates: &[u8], _ignore: bool, _c: char) {}
-
-        fn put(&mut self, _byte: u8) {}
-
-        fn unhook(&mut self) {}
-
-        fn osc_dispatch(&mut self, _params: &[&[u8]], _bell_terminated: bool) {
-            self.element = Some(Element::Osc(0, 0));
-        }
-
-        fn esc_dispatch(&mut self, _intermediates: &[u8], _ignore: bool, _byte: u8) {
-            self.element = Some(Element::Esc(0, 0));
-        }
-    }
 }
 
 #[cfg(test)]
