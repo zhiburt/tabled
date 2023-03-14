97aae40dbbf0959386b83745512382c4f5ccb256
Isaac Cloos <cloosisaac@gmail.com>
2023-03-14 10:56:45 -0400

    code cleanup

--- tabled/src/settings/split/mod.rs
+++ tabled/src/settings/split/mod.rs
@@ -1,5 +1,7 @@
 //! Split setting is a work in progress
 
+use papergrid::config::Position;
+
 use crate::records::{ExactRecords, Records, Resizable};
 
 use super::TableOption;
@@ -16,10 +18,17 @@ enum Behavior {
     Zip,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Display {
+    Clean,
+    Retain,
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Split {
     direction: Direction,
     behavior: Behavior,
+    display: Display,
     index: usize,
 }
 
@@ -28,6 +37,7 @@ impl Split {
         Split {
             direction: Direction::Column,
             behavior: Behavior::Zip,
+            display: Display::Clean,
             index,
         }
     }
@@ -36,6 +46,7 @@ impl Split {
         Split {
             direction: Direction::Row,
             behavior: Behavior::Zip,
+            display: Display::Clean,
             index,
         }
     }
@@ -53,6 +64,20 @@ impl Split {
             ..self
         }
     }
+
+    pub fn clean(self) -> Self {
+        Self {
+            display: Display::Clean,
+            ..self
+        }
+    }
+
+    pub fn retain(self) -> Self {
+        Self {
+            display: Display::Retain,
+            ..self
+        }
+    }
 }
 
 impl<R, D, Cfg> TableOption<R, D, Cfg> for Split
@@ -60,210 +85,96 @@ where
     R: Records + ExactRecords + Resizable,
 {
     fn change(&mut self, records: &mut R, _: &mut Cfg, _: &mut D) {
-        use Behavior::*;
-        use Direction::*;
-
+        // prepare many variables
+        let Split {
+            direction,
+            behavior,
+            display,
+            index: section_length,
+        } = *self;
         let columns = records.count_columns();
         let rows = records.count_rows();
-
         if columns == 0 || rows == 0 {
             return;
         }
+        let (primary_length, secondary_length) = match direction {
+            Direction::Column => (columns, rows),
+            Direction::Row => (rows, columns),
+        };
+        let sections_per_direction = ceil_div(primary_length, section_length);
+        let mut filtered_sections = 0;
 
-        match (self.direction, self.behavior) {
-            (Column, Concat) => split_column_concat(records, columns, rows, self.index),
-            (Column, Zip) => split_column_zip(records, columns, rows, self.index),
-            (Row, Concat) => split_row_concat(records, columns, rows, self.index),
-            (Row, Zip) => split_row_zip(records, columns, rows, self.index),
-        }
-    }
-}
+        // determine section over direction or vice versa based on behavior
+        let (outer_range, inner_range) = match behavior {
+            Behavior::Concat => (1..sections_per_direction, 0..secondary_length),
+            Behavior::Zip => (0..secondary_length, 1..sections_per_direction),
+        };
+        for outer_index in outer_range {
+            for inner_index in inner_range.clone() {
+                let (section_index, from_secondary_index, to_secondary_index) = match behavior {
+                    Behavior::Concat => (
+                        outer_index,
+                        inner_index,
+                        inner_index + outer_index * secondary_length - filtered_sections,
+                    ),
+                    Behavior::Zip => (
+                        inner_index,
+                        outer_index * sections_per_direction - filtered_sections,
+                        outer_index * sections_per_direction - filtered_sections + inner_index,
+                    ),
+                };
 
-fn split_column_concat<R>(records: &mut R, columns: usize, rows: usize, index: usize)
-where
-    R: Resizable + ExactRecords,
-{
-    let sections_per_row = ceil_div(columns, index);
+                // mutate table
+                match (direction, behavior) {
+                    (Direction::Column, Behavior::Concat) => records.push_row(),
+                    (Direction::Column, Behavior::Zip) => records.insert_row(to_secondary_index),
+                    (Direction::Row, Behavior::Concat) => records.push_column(),
+                    (Direction::Row, Behavior::Zip) => records.insert_column(to_secondary_index),
+                }
 
-    let mut thrown_out_sections = 0;
+                // copy cells to new location
+                let mut section_is_empty = true;
+                for to_primary_index in 0..section_length {
+                    let from_primary_index = to_primary_index + section_index * section_length;
 
-    for section in 1..sections_per_row {
-        'outer: for ref_row in 0..rows {
-            let target_row = ref_row + section * rows - thrown_out_sections;
-            records.push_row();
-
-            let mut blanks_in_section = 0;
-            for target_col in 0..index {
-                let ref_col = target_col + section * index;
-
-                if ref_col < columns {
-                    let cell_is_blank = records.get_cell((ref_row, ref_col)).as_ref() == "";
-                    if cell_is_blank {
-                        blanks_in_section += 1;
+                    if from_primary_index < primary_length {
+                        let from_position =
+                            format_position(direction, from_primary_index, from_secondary_index);
+                        if records.get_cell(from_position).as_ref() != "" {
+                            section_is_empty = false;
+                        }
+                        records.swap(
+                            from_position,
+                            format_position(direction, to_primary_index, to_secondary_index),
+                        );
                     }
-                    if (ref_col == columns - 1
-                        && sections_per_row != (columns / index)
-                        && cell_is_blank)
-                        || blanks_in_section == index
-                    {
-                        records.remove_row(target_row);
-                        thrown_out_sections += 1;
-                        continue 'outer;
-                    }
+                }
 
-                    records.swap((target_row, target_col), (ref_row, ref_col));
+                // remove empty rows/columns if cell retention is disabled
+                if section_is_empty && display == Display::Clean {
+                    match direction {
+                        Direction::Column => records.remove_row(to_secondary_index),
+                        Direction::Row => records.remove_column(to_secondary_index),
+                    }
+                    filtered_sections += 1;
                 }
             }
         }
-    }
 
-    clean_columns(records, (index..columns).rev())
-}
-
-fn split_column_zip<R>(records: &mut R, columns: usize, rows: usize, index: usize)
-where
-    R: Resizable + ExactRecords,
-{
-    let sections_per_row = ceil_div(columns, index);
-
-    let mut thrown_out_sections = 0;
-
-    for mut ref_row in 0..rows {
-        ref_row *= sections_per_row;
-
-        'outer: for section in 1..sections_per_row {
-            let target_row = ref_row + section - thrown_out_sections;
-            records.insert_row(target_row);
-
-            let mut blanks_in_section = 0;
-            for target_col in 0..index {
-                let ref_col = target_col + (section * index);
-
-                if ref_col < columns {
-                    let cell_is_blank = records.get_cell((ref_row, ref_col)).as_ref() == "";
-                    if cell_is_blank {
-                        blanks_in_section += 1;
-                    }
-                    if (ref_col == columns - 1
-                        && sections_per_row != (columns / index)
-                        && cell_is_blank)
-                        || blanks_in_section == index
-                    {
-                        records.remove_row(target_row);
-                        thrown_out_sections += 1;
-                        continue 'outer;
-                    }
-
-                    records.swap((target_row, target_col), (ref_row, ref_col));
-                }
+        // reduce table to selected index
+        for segment in (section_length..primary_length).rev() {
+            match direction {
+                Direction::Column => records.remove_column(segment),
+                Direction::Row => records.remove_row(segment),
             }
         }
     }
-
-    clean_columns(records, (index..columns).rev())
 }
 
-fn split_row_concat<R>(records: &mut R, columns: usize, rows: usize, index: usize)
-where
-    R: Resizable + ExactRecords,
-{
-    let sections_per_column = ceil_div(rows, index);
-
-    let mut thrown_out_sections = 0;
-
-    for section in 1..sections_per_column {
-        for ref_col in 0..columns {
-            let target_col = ref_col + section * columns - thrown_out_sections;
-            records.push_column();
-
-            let mut blanks_in_section = 0;
-            for target_row in 0..index {
-                let ref_row = target_row + section * index;
-
-                if ref_row < rows {
-                    let cell_is_blank = records.get_cell((ref_row, ref_col)).as_ref() == "";
-                    if cell_is_blank {
-                        blanks_in_section += 1;
-                    }
-                    if (section == sections_per_column - 1
-                        && sections_per_column != (rows / index)
-                        && cell_is_blank)
-                        || blanks_in_section == index
-                    {
-                        records.remove_column(target_col);
-                        thrown_out_sections += 1;
-                        continue;
-                    }
-
-                    records.swap((target_row, target_col), (ref_row, ref_col));
-                }
-            }
-        }
-    }
-
-    clean_rows(records, (index..rows).rev());
-}
-
-fn split_row_zip<R>(records: &mut R, columns: usize, rows: usize, index: usize)
-where
-    R: Resizable + ExactRecords,
-{
-    let sections_per_column = ceil_div(rows, index);
-
-    let mut thrown_out_sections = 0;
-
-    for mut ref_col in 0..columns {
-        ref_col = ref_col * sections_per_column - thrown_out_sections;
-
-        for section in 1..sections_per_column {
-            let target_col = ref_col + section;
-            records.insert_column(target_col);
-
-            let mut blanks_in_section = 0;
-            for target_row in 0..index {
-                let ref_row = target_row + (section * index);
-
-                if ref_row < rows {
-                    let cell_is_blank = records.get_cell((ref_row, ref_col)).as_ref() == "";
-                    if cell_is_blank {
-                        blanks_in_section += 1;
-                    }
-                    if (section == sections_per_column - 1
-                        && sections_per_column != (rows / index)
-                        && cell_is_blank)
-                        || blanks_in_section == index
-                    {
-                        records.remove_column(target_col);
-                        thrown_out_sections += 1;
-                        continue;
-                    }
-
-                    records.swap((target_row, target_col), (ref_row, ref_col));
-                }
-            }
-        }
-    }
-
-    clean_rows(records, (index..rows).rev());
-}
-
-fn clean_rows<R, I>(records: &mut R, range: I)
-where
-    R: Resizable,
-    I: Iterator<Item = usize>,
-{
-    for row in range {
-        records.remove_row(row)
-    }
-}
-
-fn clean_columns<R, I>(records: &mut R, range: I)
-where
-    R: Resizable,
-    I: Iterator<Item = usize>,
-{
-    for column in range {
-        records.remove_column(column)
+fn format_position(direction: Direction, primary_index: usize, secondary_index: usize) -> Position {
+    match direction {
+        Direction::Column => (secondary_index, primary_index),
+        Direction::Row => (primary_index, secondary_index),
     }
 }
 
