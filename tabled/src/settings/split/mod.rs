cfc891093a3690af8b77fe346865b9b3e1a138f6
Isaac Cloos <cloosisaac@gmail.com>
2023-03-15 19:24:53 -0400

    CI fixed, zip bug fixed, tests added

--- tabled/src/settings/split/mod.rs
+++ tabled/src/settings/split/mod.rs
@@ -3,6 +3,8 @@
 //!
 //! [`Table`]: crate::Table
 
+use core::ops::Range;
+
 use papergrid::config::Position;
 
 use crate::records::{ExactRecords, Records, Resizable};
@@ -233,47 +235,41 @@ where
     R: Records + ExactRecords + Resizable,
 {
     fn change(&mut self, records: &mut R, _: &mut Cfg, _: &mut D) {
-        // prepare many variables
+        // variables
         let Split {
             direction,
             behavior,
             display,
             index: section_length,
         } = *self;
-        let columns = records.count_columns();
-        let rows = records.count_rows();
-        if columns == 0 || rows == 0 || section_length == 0 {
-            return;
-        }
-        let (primary_length, secondary_length) = match direction {
-            Direction::Column => (columns, rows),
-            Direction::Row => (rows, columns),
-        };
-        let sections_per_direction = ceil_div(primary_length, section_length);
         let mut filtered_sections = 0;
 
-        // determine section over direction or vice versa based on behavior
-        let (outer_range, inner_range) = match behavior {
-            Behavior::Concat => (1..sections_per_direction, 0..secondary_length),
-            Behavior::Zip => (0..secondary_length, 1..sections_per_direction),
-        };
+        // early return check
+        if records.count_columns() == 0 || records.count_rows() == 0 || section_length == 0 {
+            return;
+        }
+
+        // computed variables
+        let (primary_length, secondary_length) = compute_length_arrangement(records, direction);
+        let sections_per_direction = ceil_div(primary_length, section_length);
+        let (outer_range, inner_range) =
+            compute_range_order(secondary_length, sections_per_direction, behavior);
+
+        // work
         for outer_index in outer_range {
+            let from_secondary_index = outer_index * sections_per_direction - filtered_sections;
             for inner_index in inner_range.clone() {
-                let (section_index, from_secondary_index, to_secondary_index) = match behavior {
-                    Behavior::Concat => (
+                let (section_index, from_secondary_index, to_secondary_index) =
+                    compute_range_variables(
                         outer_index,
                         inner_index,
-                        inner_index + outer_index * secondary_length - filtered_sections,
-                    ),
-                    Behavior::Zip => (
-                        inner_index,
-                        usize::checked_sub(outer_index * sections_per_direction, filtered_sections)
-                            .unwrap_or(0),
-                        outer_index * sections_per_direction + inner_index - filtered_sections,
-                    ),
-                };
+                        secondary_length,
+                        from_secondary_index,
+                        sections_per_direction,
+                        filtered_sections,
+                        behavior,
+                    );
 
-                // mutate table
                 match (direction, behavior) {
                     (Direction::Column, Behavior::Concat) => records.push_row(),
                     (Direction::Column, Behavior::Zip) => records.insert_row(to_secondary_index),
@@ -281,45 +277,133 @@ where
                     (Direction::Row, Behavior::Zip) => records.insert_column(to_secondary_index),
                 }
 
-                // copy cells to new location
-                let mut section_is_empty = true;
-                for to_primary_index in 0..section_length {
-                    let from_primary_index = to_primary_index + section_index * section_length;
+                let section_is_empty = copy_section(
+                    records,
+                    section_length,
+                    section_index,
+                    primary_length,
+                    from_secondary_index,
+                    to_secondary_index,
+                    direction,
+                );
 
-                    if from_primary_index < primary_length {
-                        let from_position =
-                            format_position(direction, from_primary_index, from_secondary_index);
-                        if records.get_cell(from_position).as_ref() != "" {
-                            section_is_empty = false;
-                        }
-                        records.swap(
-                            from_position,
-                            format_position(direction, to_primary_index, to_secondary_index),
-                        );
-                    }
-                }
-
-                // remove empty rows/columns if cell retention is disabled
                 if section_is_empty && display == Display::Clean {
-                    match direction {
-                        Direction::Column => records.remove_row(to_secondary_index),
-                        Direction::Row => records.remove_column(to_secondary_index),
-                    }
+                    delete(records, to_secondary_index, direction);
                     filtered_sections += 1;
                 }
             }
         }
 
-        // reduce table to selected index
-        for segment in (section_length..primary_length).rev() {
-            match direction {
-                Direction::Column => records.remove_column(segment),
-                Direction::Row => records.remove_row(segment),
-            }
+        cleanup(records, section_length, primary_length, direction);
+    }
+}
+
+/// Determine which direction should be considered the primary, and which the secondary based on direction
+fn compute_length_arrangement<R>(records: &mut R, direction: Direction) -> (usize, usize)
+where
+    R: Records + ExactRecords,
+{
+    match direction {
+        Direction::Column => (records.count_columns(), records.count_rows()),
+        Direction::Row => (records.count_rows(), records.count_columns()),
+    }
+}
+
+/// reduce the table size to the length of the index in the specified direction
+fn cleanup<R>(records: &mut R, section_length: usize, primary_length: usize, direction: Direction)
+where
+    R: Resizable,
+{
+    for segment in (section_length..primary_length).rev() {
+        match direction {
+            Direction::Column => records.remove_column(segment),
+            Direction::Row => records.remove_row(segment),
         }
     }
 }
 
+/// Delete target index row or column
+fn delete<R>(records: &mut R, target_index: usize, direction: Direction)
+where
+    R: Resizable,
+{
+    match direction {
+        Direction::Column => records.remove_row(target_index),
+        Direction::Row => records.remove_column(target_index),
+    }
+}
+
+/// copy cells to new location
+///
+/// returns if the copied section was entirely blank
+fn copy_section<R>(
+    records: &mut R,
+    section_length: usize,
+    section_index: usize,
+    primary_length: usize,
+    from_secondary_index: usize,
+    to_secondary_index: usize,
+    direction: Direction,
+) -> bool
+where
+    R: ExactRecords + Resizable,
+{
+    let mut section_is_empty = true;
+    for to_primary_index in 0..section_length {
+        let from_primary_index = to_primary_index + section_index * section_length;
+
+        if from_primary_index < primary_length {
+            let from_position =
+                format_position(direction, from_primary_index, from_secondary_index);
+            if records.get_cell(from_position).as_ref() != "" {
+                section_is_empty = false;
+            }
+            records.swap(
+                from_position,
+                format_position(direction, to_primary_index, to_secondary_index),
+            );
+        }
+    }
+    section_is_empty
+}
+
+/// determine section over direction or vice versa based on behavior
+fn compute_range_order(
+    direction_length: usize,
+    sections_per_direction: usize,
+    behavior: Behavior,
+) -> (Range<usize>, Range<usize>) {
+    match behavior {
+        Behavior::Concat => (1..sections_per_direction, 0..direction_length),
+        Behavior::Zip => (0..direction_length, 1..sections_per_direction),
+    }
+}
+
+/// helper function for shimming both behaviors to work within a single nested loop
+fn compute_range_variables(
+    outer_index: usize,
+    inner_index: usize,
+    direction_length: usize,
+    from_secondary_index: usize,
+    sections_per_direction: usize,
+    filtered_sections: usize,
+    behavior: Behavior,
+) -> (usize, usize, usize) {
+    match behavior {
+        Behavior::Concat => (
+            outer_index,
+            inner_index,
+            inner_index + outer_index * direction_length - filtered_sections,
+        ),
+        Behavior::Zip => (
+            inner_index,
+            from_secondary_index,
+            outer_index * sections_per_direction + inner_index - filtered_sections,
+        ),
+    }
+}
+
+/// utility for arguments of a position easily
 fn format_position(direction: Direction, primary_index: usize, secondary_index: usize) -> Position {
     match direction {
         Direction::Column => (secondary_index, primary_index),
@@ -327,6 +411,7 @@ fn format_position(direction: Direction, primary_index: usize, secondary_index: 
     }
 }
 
+/// ceil division utility because the std lib ceil_div isn't stable yet
 fn ceil_div(x: usize, y: usize) -> usize {
     debug_assert!(x != 0);
     1 + ((x - 1) / y)
