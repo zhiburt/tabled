3635232232bd7ee34f3680357bc08bf1e8296b47
danieleades <33452915+danieleades@users.noreply.github.com>
2023-03-17 14:17:46 +0000

    bump 'ansi-str' dependency (#288)
    Co-authored-by: daniel.eades <daniel.eades@hotmail.com>

--- papergrid/src/color/ansi_color.rs
+++ papergrid/src/color/ansi_color.rs
@@ -69,8 +69,8 @@ fn parse_ansi_color(s: &str) -> Option<AnsiColor<'static>> {
     let mut blocks = ansi_str::get_blocks(s);
     let block = blocks.next()?;
 
-    let start = block.start().to_string();
-    let end = block.end().to_string();
+    let start = block.style().start().to_string();
+    let end = block.style().end().to_string();
 
     Some(AnsiColor::new(start.into(), end.into()))
 }
