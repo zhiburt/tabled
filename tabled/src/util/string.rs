2086f17415c0a7fd4203bf2ce8a7cf2c4e0db8f8
andrii <andrii.dovzhenko.dv@gmail.com>
2024-07-13 06:04:45 +0300

    Fix the flag test

--- tabled/src/util/string.rs
+++ tabled/src/util/string.rs
@@ -201,7 +201,7 @@ mod tests {
         assert_eq!(cut_str("🏳️🏳️", 0), "");
         assert_eq!(cut_str("🏳️🏳️", 1), "🏳");
         assert_eq!(cut_str("🏳️🏳️", 2), "🏳\u{fe0f}🏳");
-        assert_eq!(string_width("🏳️🏳️"), string_width("🏳️\u{fe0f}🏳️"));
+        assert_eq!(string_width("🏳️🏳️"), string_width("🏳\u{fe0f}🏳\u{fe0f}"));
 
         assert_eq!(cut_str("🎓", 1), "�");
         assert_eq!(cut_str("🎓", 2), "🎓");
