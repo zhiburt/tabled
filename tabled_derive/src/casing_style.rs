08114e56bdeb805894b21f06529c9a5e6072b622
Maxim Zhiburt <zhiburt@gmail.com>
2025-02-28 01:00:39 +0300

    Add new rename_all options ['Upper Title Case', 'lower title case']

--- tabled_derive/src/casing_style.rs
+++ tabled_derive/src/casing_style.rs
@@ -17,6 +17,10 @@ pub enum CasingStyle {
     Lower,
     /// Keep all letters uppercase and remove word boundaries.
     Upper,
+    /// Split string by words and start each with upper latter.
+    UpperTitle,
+    /// Split string by words.
+    LowerTitle,
     /// Use the original attribute name defined in the code.
     Verbatim,
 }
@@ -36,23 +40,35 @@ impl CasingStyle {
             "snake" | "snakecase" => Ok(Snake),
             "lower" | "lowercase" => Ok(Lower),
             "upper" | "uppercase" => Ok(Upper),
+            "uppertitle" | "uppertitlecase" => Ok(UpperTitle),
+            "lowertitle" | "lowertitlecase" => Ok(LowerTitle),
             "verbatim" | "verbatimcase" => Ok(Verbatim),
-            _ => Err(Error::new(format!("unsupported casing: `{:?}`", name.value()), name.span(), Some("supported values are ['camelCase', 'kebab-case', 'PascalCase', 'SCREAMING_SNAKE_CASE', 'snake_case', 'lowercase', 'UPPERCASE', 'verbatim']".to_owned())))
+            _ => {
+                const SUPPORTED: &str = "supported values are ['camelCase', 'kebab-case', 'PascalCase', 'SCREAMING_SNAKE_CASE', 'snake_case', 'lowercase', 'UPPERCASE', 'lower title case', 'Upper Title Case', 'verbatim']";
+
+                Err(Error::new(
+                    format!("unsupported casing: `{:?}`", name.value()),
+                    name.span(),
+                    Some(SUPPORTED.to_owned()),
+                ))
+            }
         }
     }
 
-    pub fn cast(self, s: String) -> String {
+    pub fn cast(self, text: String) -> String {
         use CasingStyle::*;
 
         match self {
-            Pascal => heck::ToUpperCamelCase::to_upper_camel_case(s.as_str()),
-            Camel => heck::ToLowerCamelCase::to_lower_camel_case(s.as_str()),
-            Kebab => heck::ToKebabCase::to_kebab_case(s.as_str()),
-            Snake => heck::ToSnakeCase::to_snake_case(s.as_str()),
-            ScreamingSnake => heck::ToShoutySnakeCase::to_shouty_snake_case(s.as_str()),
-            Lower => heck::ToSnakeCase::to_snake_case(s.as_str()).replace('_', ""),
-            Upper => heck::ToShoutySnakeCase::to_shouty_snake_case(s.as_str()).replace('_', ""),
-            Verbatim => s,
+            Pascal => heck::ToUpperCamelCase::to_upper_camel_case(text.as_str()),
+            Camel => heck::ToLowerCamelCase::to_lower_camel_case(text.as_str()),
+            Kebab => heck::ToKebabCase::to_kebab_case(text.as_str()),
+            Snake => heck::ToSnakeCase::to_snake_case(text.as_str()),
+            ScreamingSnake => heck::ToShoutySnakeCase::to_shouty_snake_case(text.as_str()),
+            Lower => heck::ToSnakeCase::to_snake_case(text.as_str()).replace('_', ""),
+            Upper => heck::ToShoutySnakeCase::to_shouty_snake_case(text.as_str()).replace('_', ""),
+            LowerTitle => heck::ToSnakeCase::to_snake_case(text.as_str()).replace('_', " "),
+            UpperTitle => heck::ToTitleCase::to_title_case(text.as_str()),
+            Verbatim => text,
         }
     }
 }
