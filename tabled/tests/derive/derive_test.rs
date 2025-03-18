b0c329254d6f0be282f45f42a12f0676ee2d5f8e
Maxim Zhiburt <zhiburt@gmail.com>
2025-03-18 23:54:54 +0300

    Add tests for derive map

--- tabled/tests/derive/derive_test.rs
+++ tabled/tests/derive/derive_test.rs
@@ -1836,6 +1836,79 @@ mod test_display_enum {
     );
 }
 
+test_table!(
+    test_map_path,
+    {
+        #[derive(Tabled)]
+        struct User {
+            id: usize,
+            #[tabled(map = "password_string")]
+            p: Pass<usize>,
+        }
+
+        struct Pass<T>([T; 4]);
+
+        fn password_string(p: &Pass<usize>) -> String {
+            p.0.iter().sum::<usize>().to_string()
+        }
+
+        let data = [
+            User { id: 0, p: Pass([0, 1, 2, 3]) },
+            User { id: 1, p: Pass([1, 1, 2, 3]) },
+        ];
+
+        Table::new(data)
+    },
+    "+----+---+"
+    "| id | p |"
+    "+----+---+"
+    "| 0  | 6 |"
+    "+----+---+"
+    "| 1  | 7 |"
+    "+----+---+"
+);
+
+test_table!(
+    test_map_inline,
+    {
+        #[derive(Tabled)]
+        struct User {
+            id: usize,
+            #[tabled(map(Pass, "pass"))]
+            #[tabled(inline)]
+            p: isize,
+        }
+
+        #[derive(Tabled)]
+        struct Pass(String, String, String);
+
+        fn pass(p: &isize) -> Pass {
+            Pass (
+                (*p > 0).then_some(String::from("+")).unwrap_or_default(),
+                (*p < 0).then_some(String::from("-")).unwrap_or_default(),
+                (*p == 0).then_some(String::from("=")).unwrap_or_default(),
+            )
+        }
+
+        let data = [
+            User { id: 0, p: 0 },
+            User { id: 1, p: 1 },
+            User { id: 1, p: -1 },
+        ];
+
+        Table::new(data)
+    },
+    "+----+---+---+---+"
+    "| id | 0 | 1 | 2 |"
+    "+----+---+---+---+"
+    "| 0  |   |   | = |"
+    "+----+---+---+---+"
+    "| 1  | + |   |   |"
+    "+----+---+---+---+"
+    "| 1  |   | - |   |"
+    "+----+---+---+---+"
+);
+
 // TODO: Shall we support a 'display_type' for #inlined structs??
 // Seems like we have to?
 // But there's a small issue currently - we treat enum variants as field currently in a parsing stage.
